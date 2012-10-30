fe000c2346f425fb76f14b36e5cb8360929f8a1e
Chiao Cheng <chiaocheng@google.com>
2012-10-30 09:44:45 -0700

    Move PhoneNumberFormatter to common.
    Bug: 6993891
    Change-Id: I21b3afb81855ae728b9e20f8af8632c9cd9f4e58

--- src/com/android/dialer/dialpad/DialpadFragment.java
+++ src/com/android/dialer/dialpad/DialpadFragment.java
@@ -69,7 +69,7 @@ import android.widget.TextView;
 
 import com.android.contacts.common.CallUtil;
 import com.android.contacts.common.GeoUtil;
-import com.android.contacts.util.PhoneNumberFormatter;
+import com.android.contacts.common.util.PhoneNumberFormatter;
 import com.android.contacts.common.util.StopWatch;
 import com.android.dialer.DialtactsActivity;
 import com.android.dialer.R;
