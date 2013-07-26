LineageOS/android_packages_apps_Contacts

Store phone blacklist in content provider (3/4)
Change-Id: I7b42e872b1ccaba7eff6f6c4903e8a3612aa8375

src/com/android/contacts/calllog/ContactInfoHelper.java
@@ -16,6 +16,7 @@
 
 package com.android.contacts.calllog;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -23,6 +24,7 @@ import android.net.Uri;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.PhoneLookup;
 import android.provider.Settings;
+import android.provider.Telephony;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.widget.Toast;
@@ -36,10 +38,6 @@ import com.android.contacts.util.UriUtils;
 public class ContactInfoHelper {
     private final Context mContext;
     private final String mCurrentCountryIso;
-
-    // Blacklist support
-    private static final String INSERT_BLACKLIST = "com.android.phone.INSERT_BLACKLIST";
-    private static final String BLACKLIST_NUMBER = "number";
 
     public ContactInfoHelper(Context context, String currentCountryIso) {
         mContext = context;
@@ -227,13 +225,17 @@ public class ContactInfoHelper {
      * @param number the number to be blacklisted
      */
     public void addNumberToBlacklist(String number) {
-                Intent intent = new Intent(INSERT_BLACKLIST);
-        intent.putExtra(BLACKLIST_NUMBER, number);
-        mContext.sendBroadcast(intent);
+        ContentValues cv = new ContentValues();
+        cv.put(Telephony.Blacklist.PHONE_MODE, 1);
 
-        // Give the user some feedback
-        String message = mContext.getString(R.string.toast_added_to_blacklist, number);
-        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+        Uri uri = Uri.withAppendedPath(Telephony.Blacklist.CONTENT_URI, number);
+        int count = mContext.getContentResolver().update(uri, cv, null, null);
+
+        if (count != 0) {
+            // Give the user some feedback
+            String message = mContext.getString(R.string.toast_added_to_blacklist, number);
+            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**

LineageOS/android_packages_apps_Dialer

src/com/android/dialer/calllog/ContactInfoHelper.java
@@ -16,6 +16,7 @@
 
 package com.android.dialer.calllog;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -23,6 +24,7 @@ import android.net.Uri;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.PhoneLookup;
 import android.provider.Settings;
+import android.provider.Telephony;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.widget.Toast;
@@ -36,10 +38,6 @@ import com.android.contacts.common.util.UriUtils;
 public class ContactInfoHelper {
     private final Context mContext;
     private final String mCurrentCountryIso;
-
-    // Blacklist support
-    private static final String INSERT_BLACKLIST = "com.android.phone.INSERT_BLACKLIST";
-    private static final String BLACKLIST_NUMBER = "number";
 
     public ContactInfoHelper(Context context, String currentCountryIso) {
         mContext = context;
@@ -236,12 +234,16 @@ public class ContactInfoHelper {
      * @param number the number to be blacklisted
      */
     public void addNumberToBlacklist(String number) {
-                Intent intent = new Intent(INSERT_BLACKLIST);
-        intent.putExtra(BLACKLIST_NUMBER, number);
-        mContext.sendBroadcast(intent);
+        ContentValues cv = new ContentValues();
+        cv.put(Telephony.Blacklist.PHONE_MODE, 1);
 
-        // Give the user some feedback
-        String message = mContext.getString(R.string.toast_added_to_blacklist, number);
-        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+        Uri uri = Uri.withAppendedPath(Telephony.Blacklist.CONTENT_FILTER_BYNUMBER_URI, number);
+        int count = mContext.getContentResolver().update(uri, cv, null, null);
+
+        if (count != 0) {
+            // Give the user some feedback
+            String message = mContext.getString(R.string.toast_added_to_blacklist, number);
+            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+        }
     }
 }