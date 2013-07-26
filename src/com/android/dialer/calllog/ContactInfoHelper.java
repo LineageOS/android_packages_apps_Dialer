LineageOS/android_packages_apps_Contacts

Allow blacklisting numbers from the call stats details menu.
Change-Id: I840c382afb561805fd74cbd696a80d3bca4be041
JIRA:CYAN-910

src/com/android/contacts/calllog/ContactInfoHelper.java
@@ -17,13 +17,16 @@
 package com.android.contacts.calllog;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.PhoneLookup;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
+import android.widget.Toast;
 
+import com.android.contacts.R;
 import com.android.contacts.util.UriUtils;
 
 /**
@@ -32,6 +35,10 @@ import com.android.contacts.util.UriUtils;
 public class ContactInfoHelper {
     private final Context mContext;
     private final String mCurrentCountryIso;
+
+    // Blacklist support
+    private static final String INSERT_BLACKLIST = "com.android.phone.INSERT_BLACKLIST";
+    private static final String BLACKLIST_NUMBER = "number";
 
     public ContactInfoHelper(Context context, String currentCountryIso) {
         mContext = context;
@@ -211,5 +218,20 @@ public class ContactInfoHelper {
             countryIso = mCurrentCountryIso;
         }
         return PhoneNumberUtils.formatNumber(number, normalizedNumber, countryIso);
+    }
+
+    /**
+     * Requests the given number to be added to the phone blacklist
+     *
+     * @param number the number to be blacklisted
+     */
+    public void addNumberToBlacklist(String number) {
+                Intent intent = new Intent(INSERT_BLACKLIST);
+        intent.putExtra(BLACKLIST_NUMBER, number);
+        mContext.sendBroadcast(intent);
+
+        // Give the user some feedback
+        String message = mContext.getString(R.string.toast_added_to_blacklist, number);
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
     }
 }

LineageOS/android_packages_apps_Dialer

src/com/android/dialer/calllog/ContactInfoHelper.java
@@ -17,6 +17,7 @@
 package com.android.dialer.calllog;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract.Contacts;
@@ -24,7 +25,9 @@ import android.provider.ContactsContract.PhoneLookup;
 import android.provider.Settings;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
-
+import android.widget.Toast;
+    
+import com.android.dialer.R;
 import com.android.contacts.common.util.UriUtils;
 
 /**
@@ -33,6 +36,10 @@ import com.android.contacts.common.util.UriUtils;
 public class ContactInfoHelper {
     private final Context mContext;
     private final String mCurrentCountryIso;
+
+    // Blacklist support
+    private static final String INSERT_BLACKLIST = "com.android.phone.INSERT_BLACKLIST";
+    private static final String BLACKLIST_NUMBER = "number";
 
     public ContactInfoHelper(Context context, String currentCountryIso) {
         mContext = context;
@@ -221,5 +228,20 @@ public class ContactInfoHelper {
     public boolean canBlacklistCalls() {
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PHONE_BLACKLIST_ENABLED, 1) != 0;
+    }
+
+    /**
+     * Requests the given number to be added to the phone blacklist
+     *
+     * @param number the number to be blacklisted
+     */
+    public void addNumberToBlacklist(String number) {
+                Intent intent = new Intent(INSERT_BLACKLIST);
+        intent.putExtra(BLACKLIST_NUMBER, number);
+        mContext.sendBroadcast(intent);
+
+        // Give the user some feedback
+        String message = mContext.getString(R.string.toast_added_to_blacklist, number);
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
     }
 }