ef2649806b2116ff3ff145375e65410fd70bfd9f
Danny Baumann <dannybaumann@web.de>
2013-07-29 15:33:46 +0200

    Enable contact photo preloading.
    Avoids the delay occuring otherwise when checking into call details.
    
    Change-Id: Ie48285bb03fde0434429079f141ec57dc75a65b9

--- src/com/android/dialer/DialerApplication.java
+++ src/com/android/dialer/DialerApplication.java
@@ -4,13 +4,29 @@ package com.android.dialer;
 
 import android.app.Application;
 
+import com.android.contacts.common.ContactPhotoManager;
 import com.android.contacts.common.extensions.ExtensionsFactory;
 
 public class DialerApplication extends Application {
+    private ContactPhotoManager mContactPhotoManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
         ExtensionsFactory.init(getApplicationContext());
+    }
+
+    @Override
+    public Object getSystemService(String name) {
+        if (ContactPhotoManager.CONTACT_PHOTO_SERVICE.equals(name)) {
+            if (mContactPhotoManager == null) {
+                mContactPhotoManager = ContactPhotoManager.createContactPhotoManager(this);
+                registerComponentCallbacks(mContactPhotoManager);
+                mContactPhotoManager.preloadPhotosInBackground();
+            }
+            return mContactPhotoManager;
+        }
+
+        return super.getSystemService(name);
     }
 }
