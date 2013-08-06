a4b9716944d08578cf457a5207cf749c465d914c
kenshin <foufou33@gmail.com>
2013-08-06 17:01:07 -0400

    Add lookup in Call logs to lookupNumber
    This patch add a lookup function that tries to finda match in the Call
    log (after oll ther posibilities are exhausted) and returns that
    information to be put in cache.
    
    Change-Id: I6f52ed3ea404dcc7a356850f782b2972c4844ed9

--- src/com/android/dialer/calllog/ContactInfoHelper.java
+++ src/com/android/dialer/calllog/ContactInfoHelper.java
@@ -21,6 +21,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.CallLog;
+import android.provider.CallLog.Calls;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.PhoneLookup;
 import android.provider.Settings;
@@ -82,6 +84,11 @@ public class ContactInfoHelper {
             if (phoneInfo == null || phoneInfo == ContactInfo.EMPTY) {
                 // Check whether the phone number has been saved as an "Internet call" number.
                 phoneInfo = queryContactInfoForSipAddress(number);
+
+                if (phoneInfo == null || phoneInfo == ContactInfo.EMPTY) {
+                    //Check contact in Calllog may we have some info there
+                    phoneInfo = queryContactInfoForPhoneNumberFromCallLog(number, countryIso);
+                }
             }
             info = phoneInfo;
         }
@@ -197,6 +204,66 @@ public class ContactInfoHelper {
         return info;
     }
 
+
+    /**
+     * Determines the contact information stored in the call log for the given phone number.
+     * <p>
+     * It returns the contact info if found.
+     * <p>
+     * If no contact corresponds to the given phone number, returns {@link ContactInfo#EMPTY}.
+     * <p>
+     * If the lookup fails for some other reason, it returns null.
+     */
+    private ContactInfo queryContactInfoForPhoneNumberFromCallLog(String number, String countryIso) {
+        final ContactInfo info;
+        String contactNumber = number;
+        if (!TextUtils.isEmpty(countryIso)) {
+            // Normalize the number: this is needed because the PhoneLookup query below does not
+            // accept a country code as an input.
+            String numberE164 = PhoneNumberUtils.formatNumberToE164(number, countryIso);
+            if (!TextUtils.isEmpty(numberE164)) {
+                // Only use it if the number could be formatted to E164.
+                contactNumber = numberE164;
+            }
+        }
+
+        // The "contactNumber" is a regular phone number, so use the CallLog table.
+        Uri uri = Uri.withAppendedPath(CallLog.Calls.CONTENT_FILTER_URI, Uri.encode(contactNumber));
+        //if we ought to find something it would be in incmoning/missed calls
+        String selectionClause = " " + CallLog.Calls.TYPE + " = " + CallLog.Calls.INCOMING_TYPE +
+                                 " OR " + CallLog.Calls.TYPE + " = " + CallLog.Calls.MISSED_TYPE;
+        Cursor c = mContext.getContentResolver().query(
+                uri,
+                CallLogQuery._PROJECTION,
+                selectionClause,
+                null,
+                Calls.DEFAULT_SORT_ORDER + " LIMIT 1");
+
+        if (c != null) {
+            try {
+                if (c.moveToFirst()){
+                    info = new ContactInfo();
+                    info.lookupUri = UriUtils.parseUriOrNull(c.getString(CallLogQuery.CACHED_LOOKUP_URI));
+                    info.name = c.getString(CallLogQuery.CACHED_NAME);
+                    info.type = c.getInt(CallLogQuery.CACHED_NUMBER_TYPE);
+                    info.label = c.getString(CallLogQuery.CACHED_NUMBER_LABEL);
+                    String matchedNumber = c.getString(CallLogQuery.CACHED_MATCHED_NUMBER);
+                    info.number = matchedNumber == null ? c.getString(CallLogQuery.NUMBER) : matchedNumber;
+                    info.normalizedNumber = c.getString(CallLogQuery.CACHED_NORMALIZED_NUMBER);
+                    info.photoId = c.getLong(CallLogQuery.CACHED_PHOTO_ID);
+                    info.photoUri = null;  // We do not cache the photo URI.
+                    info.formattedNumber = formatPhoneNumber(info.number, null, countryIso);
+                } else {
+                    info = ContactInfo.EMPTY;
+                }
+            }finally {
+                c.close();
+            }
+        } else {
+            info = null;
+        }
+        return info;
+    }
     /**
      * Format the given phone number
      *
