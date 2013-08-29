2e5b34b14181f367ec90198f5cbcd39f261b7bf5
Jay Shrauner <shrauner@google.com>
2013-08-29 10:52:40 -0700

    Refactor SearchFragment
    Refactor SearchFragment into a base SearchFragment and an
    implementation RegularSearchFragment. This allows SmartDialSearchFragment
    to inherit directly from the base.
    
    Bug:
    Change-Id: Iae9cd11ac733445c76d390c35fa778d942707b57

--- src/com/android/dialer/list/SearchFragment.java
+++ src/com/android/dialer/list/SearchFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.dialer;
+package com.android.dialer.list;
 
 import android.app.Activity;
 import android.widget.AbsListView;
@@ -26,18 +26,7 @@ import com.android.dialer.list.OnListFragmentScrolledListener;
 
 public class SearchFragment extends PhoneNumberPickerFragment {
 
-    private static final int SEARCH_DIRECTORY_RESULT_LIMIT = 5;
-
     private OnListFragmentScrolledListener mActivityScrollListener;
-
-    public SearchFragment() {
-        configureDirectorySearch();
-    }
-
-    public void configureDirectorySearch() {
-        setDirectorySearchEnabled(true);
-        setDirectoryResultLimit(SEARCH_DIRECTORY_RESULT_LIMIT);
-    }
 
     @Override
     public void onAttach(Activity activity) {
