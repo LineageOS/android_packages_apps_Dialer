49e377c371007d6659bf87739b627e57753d51be
Danny Baumann <dannybaumann@web.de>
2013-09-25 08:28:47 +0200

    Refresh adapter on resuming, so that call dates are updated.
    When not doing this, the call dates might be wrong (they might e.g. say
    '5 minutes ago' even though the call was 2 hours ago).
    
    Change-Id: I44b797e7d09fcfe5ad1745e6b4d2cba2639c1650
    JIRA:CYAN-1455

--- src/com/android/dialer/calllog/CallLogFragment.java
+++ src/com/android/dialer/calllog/CallLogFragment.java
@@ -417,9 +417,12 @@ public class CallLogFragment extends ListFragment
             mAdapter.invalidateCache();
             startCallsQuery();
             startVoicemailStatusQuery();
-            updateOnEntry();
             mRefreshDataRequired = false;
+        } else {
+            // make adapter refresh, so call dates are updated
+            mAdapter.notifyDataSetChanged();
         }
+        updateOnEntry();
     }
 
     /** Updates call data and notification state while leaving the call log tab. */
