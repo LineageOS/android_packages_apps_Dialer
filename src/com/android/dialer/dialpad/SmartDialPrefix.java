80bc71b960868d2be1c559ed57918bc26b697909
Michael Bestas <mikeioannina@gmail.com>
2014-01-29 03:12:24 +0200

    Dialer: Add Greek T9 support
    PS2: Fix build
    PS3: Fix country
    PS4-PS5: Complete greek character table
    
    Change-Id: I006745075cbb83b23a6bda170032d146d9bc5aea

--- src/com/android/dialer/dialpad/SmartDialPrefix.java
+++ src/com/android/dialer/dialpad/SmartDialPrefix.java
@@ -94,6 +94,8 @@ public class SmartDialPrefix {
         String locale = context.getResources().getConfiguration().locale.getCountry();
         if (locale.equals("RU")) {
             mMap = new RussianSmartDialMap();
+        } else if (locale.equals("GR")) {
+            mMap = new GreekSmartDialMap();
         } else {
             mMap = new LatinSmartDialMap();
         }
