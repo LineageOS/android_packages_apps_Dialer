511e12c5f4747643c9683114038e129363a95277
repo sync <ihab@google.com>
2014-03-25 13:14:24 -0700

    DO NOT MERGE. One-time patch change to resolve differences where klp-modular-dev became out of sync with klp-dev due to failures of automatic merges. We decided it is far better to submit this to fix the build rather than spending a bunch of time doing forensics on the history of automated merges into klp-modular-dev.
    Change-Id: I66ff89237b3308a919f711bc10425a7b9688c24b

--- InCallUI/src/com/android/incallui/InCallActivity.java
+++ InCallUI/src/com/android/incallui/InCallActivity.java
@@ -21,6 +21,7 @@ import com.android.services.telephony.common.Call.State;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.DialogInterface.OnCancelListener;
@@ -308,6 +309,10 @@ public class InCallActivity extends Activity {
         InCallPresenter.getInstance().getProximitySensor().onConfigurationChanged(config);
     }
 
+    public CallButtonFragment getCallButtonFragment() {
+        return mCallButtonFragment;
+    }
+
     private void internalResolveIntent(Intent intent) {
         final String action = intent.getAction();
 
@@ -365,7 +370,7 @@ public class InCallActivity extends Activity {
         if (mDialpadFragment == null) {
             mDialpadFragment = (DialpadFragment) getFragmentManager()
                     .findFragmentById(R.id.dialpadFragment);
-            mDialpadFragment.getView().setVisibility(View.INVISIBLE);
+            getFragmentManager().beginTransaction().hide(mDialpadFragment).commit();
         }
 
         if (mConferenceManagerFragment == null) {
@@ -398,13 +403,15 @@ public class InCallActivity extends Activity {
     }
 
     public void displayDialpad(boolean showDialpad) {
+        final FragmentTransaction ft = getFragmentManager().beginTransaction();
         if (showDialpad) {
-            mDialpadFragment.setVisible(true);
-            mCallCardFragment.setVisible(false);
+            ft.setCustomAnimations(R.anim.incall_dialpad_slide_in, 0);
+            ft.show(mDialpadFragment);
         } else {
-            mDialpadFragment.setVisible(false);
-            mCallCardFragment.setVisible(true);
+            ft.setCustomAnimations(0, R.anim.incall_dialpad_slide_out);
+            ft.hide(mDialpadFragment);
         }
+        ft.commitAllowingStateLoss();
 
         InCallPresenter.getInstance().getProximitySensor().onDialpadVisible(showDialpad);
     }
