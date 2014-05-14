4dfd7129ab04853504fe0c050e982db7b178b643
Tyler Gunn <tgunn@google.com>
2014-05-14 15:12:42 -0700

    Adding day group headings in the call log which group call log entries under 4 headings, "Today", "Yesterday", "Last week", "Other".
    Bug: 13962594
    Change-Id: I6f7cfb93ca03bbbe0c3e4736efb3b8a4244c1dba

--- src/com/android/dialer/calllog/CallLogGroupBuilder.java
+++ src/com/android/dialer/calllog/CallLogGroupBuilder.java
@@ -19,21 +19,73 @@ package com.android.dialer.calllog;
 import android.database.Cursor;
 import android.provider.CallLog.Calls;
 import android.telephony.PhoneNumberUtils;
+import android.text.format.Time;
 
 import com.android.common.widget.GroupingListAdapter;
+import com.android.contacts.common.util.DateUtils;
 import com.android.contacts.common.util.PhoneNumberHelper;
 
 import com.google.common.annotations.VisibleForTesting;
 
 /**
- * Groups together calls in the call log.
+ * Groups together calls in the call log.  The primary grouping attempts to group together calls
+ * to and from the same number into a single row on the call log.
+ * A secondary grouping assigns calls, grouped via the primary grouping, to "day groups".  The day
+ * groups provide a means of identifying the calls which occurred "Today", "Yesterday", "Last week",
+ * or "Other".
  * <p>
  * This class is meant to be used in conjunction with {@link GroupingListAdapter}.
  */
 public class CallLogGroupBuilder {
     public interface GroupCreator {
+
+        /**
+         * Defines the interface for adding a group to the call log.
+         * The primary group for a call log groups the calls together based on the number which was
+         * dialed.
+         * @param cursorPosition The starting position of the group in the cursor.
+         * @param size The size of the group.
+         * @param expanded Whether the group is expanded; always false for the call log.
+         */
         public void addGroup(int cursorPosition, int size, boolean expanded);
+
+        /**
+         * Defines the interface for tracking the day group each call belongs to.  Calls in a call
+         * group are assigned the same day group as the first call in the group.  The day group
+         * assigns calls to the buckets: Today, Yesterday, Last week, and Other
+         *
+         * @param rowId The row Id of the current call.
+         * @param dayGroup The day group the call belongs in.
+         */
+        public void setDayGroup(long rowId, int dayGroup);
+
+        /**
+         * Defines the interface for clearing the day groupings information on rebind/regroup.
+         */
+        public void clearDayGroups();
     }
+
+    /**
+     * Day grouping for call log entries used to represent no associated day group.  Used primarily
+     * when retrieving the previous day group, but there is no previous day group (i.e. we are at
+     * the start of the list).
+     */
+    public static final int DAY_GROUP_NONE = -1;
+
+    /** Day grouping for calls which occurred today. */
+    public static final int DAY_GROUP_TODAY = 0;
+
+    /** Day grouping for calls which occurred yesterday. */
+    public static final int DAY_GROUP_YESTERDAY = 1;
+
+    /** Day grouping for calls which occurred last week. */
+    public static final int DAY_GROUP_LAST_WEEK = 2;
+
+    /** Day grouping for calls which occurred before last week. */
+    public static final int DAY_GROUP_OTHER = 3;
+
+    /** Instance of the time object used for time calculations. */
+    private static final Time TIME = new Time();
 
     /** The object on which the groups are created. */
     private final GroupCreator mGroupCreator;
@@ -59,18 +111,33 @@ public class CallLogGroupBuilder {
             return;
         }
 
+        // Clear any previous day grouping information.
+        mGroupCreator.clearDayGroups();
+
+        // Get current system time, used for calculating which day group calls belong to.
+        long currentTime = System.currentTimeMillis();
+
         int currentGroupSize = 1;
         cursor.moveToFirst();
         // The number of the first entry in the group.
         String firstNumber = cursor.getString(CallLogQuery.NUMBER);
         // This is the type of the first call in the group.
         int firstCallType = cursor.getInt(CallLogQuery.CALL_TYPE);
+
+        // Determine the day group for the first call in the cursor.
+        final long firstDate = cursor.getLong(CallLogQuery.DATE);
+        final long firstRowId = cursor.getLong(CallLogQuery.ID);
+        int currentGroupDayGroup = getDayGroup(firstDate, currentTime);
+        mGroupCreator.setDayGroup(firstRowId, currentGroupDayGroup);
+
         while (cursor.moveToNext()) {
             // The number of the current row in the cursor.
             final String currentNumber = cursor.getString(CallLogQuery.NUMBER);
             final int callType = cursor.getInt(CallLogQuery.CALL_TYPE);
             final boolean sameNumber = equalNumbers(firstNumber, currentNumber);
             final boolean shouldGroup;
+            final long currentCallId = cursor.getLong(CallLogQuery.ID);
+            final long date = cursor.getLong(CallLogQuery.DATE);
 
             if (!sameNumber) {
                 // Should only group with calls from the same number.
@@ -88,6 +155,11 @@ public class CallLogGroupBuilder {
                 // the group until we find a call that does not match.
                 currentGroupSize++;
             } else {
+                // The call group has changed, so determine the day group for the new call group.
+                // This ensures all calls grouped together in the call log are assigned the same
+                // day group.
+                currentGroupDayGroup = getDayGroup(date, currentTime);
+
                 // Create a group for the previous set of calls, excluding the current one, but do
                 // not create a group for a single call.
                 if (currentGroupSize > 1) {
@@ -99,6 +171,9 @@ public class CallLogGroupBuilder {
                 firstNumber = currentNumber;
                 firstCallType = callType;
             }
+
+            // Save the day group associated with the current call.
+            mGroupCreator.setDayGroup(currentCallId, currentGroupDayGroup);
         }
         // If the last set of calls at the end of the call log was itself a group, create it now.
         if (currentGroupSize > 1) {
@@ -153,5 +228,26 @@ public class CallLogGroupBuilder {
         }
 
         return userinfo1.equals(userinfo2) && rest1.equalsIgnoreCase(rest2);
+    }
+
+    /**
+     * Given a call date and the current date, determine which date group the call belongs in.
+     *
+     * @param date The call date.
+     * @param now The current date.
+     * @return The date group the call belongs in.
+     */
+    private int getDayGroup(long date, long now) {
+        int days = DateUtils.getDayDifference(TIME, date, now);
+
+        if (days == 0) {
+            return DAY_GROUP_TODAY;
+        } else if (days == 1) {
+            return DAY_GROUP_YESTERDAY;
+        } else if (days > 1 && days <=7) {
+            return DAY_GROUP_LAST_WEEK;
+        } else {
+            return DAY_GROUP_OTHER;
+        }
     }
 }
