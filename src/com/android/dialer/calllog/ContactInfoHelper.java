acbd701e9baa04c13ee21098fdf789cdc5a3b496
Adnan <adnan@cyngn.com>
2014-08-04 15:08:26 -0700

    Dialer: Handle "Unknown" and null numbers when adding to blacklist.
    Change-Id: I819968e06387b2cf4ddd9e9d47fdf87b6099a98b
    JIRA: 278
    Issue: https://jira.cyanogenmod.org/browse/BACON-278

--- src/com/android/dialer/calllog/ContactInfoHelper.java
+++ src/com/android/dialer/calllog/ContactInfoHelper.java
@@ -280,6 +280,12 @@ public class ContactInfoHelper {
      * @param number the number to be blacklisted
      */
     public void addNumberToBlacklist(String number) {
+        if (TextUtils.isEmpty(number)) {
+            String message = mContext.getString(R.string.toast_nan_in_blacklist);
+            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         ContentValues cv = new ContentValues();
         cv.put(Telephony.Blacklist.PHONE_MODE, 1);
 
