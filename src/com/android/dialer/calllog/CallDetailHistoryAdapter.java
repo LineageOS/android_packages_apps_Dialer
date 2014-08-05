07f4d58e8f3e4812cbef4e10b9eaa4d31177dd9f
emancebo <emancebo@cyngn.com>
2014-08-04 17:07:05 -0700

    Call recording service implementation, show recordings and allow playback in call history
    Change-Id: I56bf6857d87260e20b6a84c4db6e463b0fd87dcc

--- src/com/android/dialer/calllog/CallDetailHistoryAdapter.java
+++ src/com/android/dialer/calllog/CallDetailHistoryAdapter.java
@@ -23,10 +23,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.dialer.PhoneCallDetails;
 import com.android.dialer.R;
+import com.android.dialer.util.CallRecordingPlayer;
+import com.android.services.callrecorder.common.CallRecording;
+import com.android.services.callrecorder.CallRecorderService;
+import com.android.services.callrecorder.CallRecordingDataStore;
+
+import java.util.List;
 
 /**
  * Adapter for a ListView containing history items from the details of a call.
@@ -59,9 +66,14 @@ public class CallDetailHistoryAdapter extends BaseAdapter {
         }
     };
 
+    private CallRecordingDataStore mCallRecordingDataStore;
+    private CallRecordingPlayer mCallRecordingPlayer;
+
     public CallDetailHistoryAdapter(Context context, LayoutInflater layoutInflater,
             CallTypeHelper callTypeHelper, PhoneCallDetails[] phoneCallDetails,
-            boolean showVoicemail, boolean showCallAndSms, View controls) {
+            boolean showVoicemail, boolean showCallAndSms, View controls,
+            CallRecordingDataStore callRecordingDataStore,
+            CallRecordingPlayer callRecordingPlayer) {
         mContext = context;
         mLayoutInflater = layoutInflater;
         mCallTypeHelper = callTypeHelper;
@@ -69,6 +81,8 @@ public class CallDetailHistoryAdapter extends BaseAdapter {
         mShowVoicemail = showVoicemail;
         mShowCallAndSms = showCallAndSms;
         mControls = controls;
+        mCallRecordingDataStore = callRecordingDataStore;
+        mCallRecordingPlayer = callRecordingPlayer;
     }
 
     @Override
@@ -155,6 +169,22 @@ public class CallDetailHistoryAdapter extends BaseAdapter {
         } else {
             durationView.setVisibility(View.VISIBLE);
             durationView.setText(formatDuration(details.duration));
+        }
+
+        // do this synchronously to prevent recordings from "popping in"
+        // after detail item is displayed
+        if (CallRecorderService.isEnabled(mContext)) {
+            mCallRecordingDataStore.open(mContext); // opens unless already open
+            List<CallRecording> recordings =
+                    mCallRecordingDataStore.getRecordings(details.number.toString(), details.date);
+
+            ViewGroup playbackView =
+                    (ViewGroup) result.findViewById(R.id.recording_playback_layout);
+            playbackView.removeAllViews();
+            for (CallRecording recording : recordings) {
+                Button button = mCallRecordingPlayer.createPlaybackButton(mContext, recording);
+                playbackView.addView(button);
+            }
         }
 
         return result;
