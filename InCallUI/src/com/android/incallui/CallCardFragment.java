8f841b62de6bff276799431e3405bbc7b8a333b3
Yorke Lee <yorkelee@google.com>
2014-08-18 12:07:32 -0700

    Fix for overlapping call state label
    Canceling the animation is not enough to force a redraw of the call state label.
    Set the text to null instead.
    
    Bug: 17073203
    Change-Id: Ib731e18a0d2311c0cf9f41953fadfa4ec019310c

--- InCallUI/src/com/android/incallui/CallCardFragment.java
+++ InCallUI/src/com/android/incallui/CallCardFragment.java
@@ -510,6 +510,7 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
             if (callStateAnimation != null) {
                 callStateAnimation.cancel();
             }
+            mCallStateLabel.setText(null);
             mCallStateLabel.setAlpha(0);
             mCallStateLabel.setVisibility(View.GONE);
 
