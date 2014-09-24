cae2e7f32726933d351fc75fee72ecf63f71b00a
Suchand Ghosh <suchan@codeaurora.org>
2014-09-24 10:44:23 +0530

    IMS: Clean up Conference URI system property.
    Remove system property "persist.dbg.ims_volte_enable".
    Add platform configuration flag and user's enhanced 4G
    LTE setting to decide IMS capability.
    
    Change-Id: I1ee4dfb8e73ae7bf72a3daafef6957736a560606
    CRs-Fixed: 727689

--- Android.mk
+++ Android.mk
@@ -27,7 +27,9 @@ LOCAL_AAPT_FLAGS := \
     --extra-packages com.android.contacts.common \
     --extra-packages com.android.phone.common
 
-LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_JAVA_LIBRARIES := telephony-common \
+                        ims-common
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
     com.android.services.telephony.common \
     com.android.vcard \
