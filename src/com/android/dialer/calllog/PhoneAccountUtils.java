0e82fca41425c474499548de72acbc77a7fd2f66
Nancy Chen <nancychen@google.com>
2014-11-12 09:59:54 -0800

    Use the highlight color instead of the tint color
    The tint color of a phone account only refers to the color of the icon,
    the highlight color is the general display color used for text,
    backgrounds etc. We should use highlight color in Dialer
    
    Bug: 18192071
    Change-Id: Ia7e1c6cf055357df98b2b2f481b32a727d1f6d61

--- src/com/android/dialer/calllog/PhoneAccountUtils.java
+++ src/com/android/dialer/calllog/PhoneAccountUtils.java
@@ -73,7 +73,7 @@ public class PhoneAccountUtils {
      */
     public static int getAccountColor(Context context, PhoneAccountHandle accountHandle) {
         PhoneAccount account = getAccountOrNull(context, accountHandle);
-        return account == null ? PhoneAccount.NO_COLOR : account.getIconTint();
+        return account == null ? PhoneAccount.NO_COLOR : account.getHighlightColor();
     }
 
     /**
