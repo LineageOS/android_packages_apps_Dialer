491f91eedb36d0f02187b16eb59bf49b3756a015
Roman Birg <roman@cyngn.com>
2014-11-20 16:19:38 -0800

    Dialer: blacklisted call tweaks
    * Make icon like the missed call icon, but black.
    * Also list blacklisted calls in missed calls tab
    
    Change-Id: I0b68325b66b12988f24ff9e4b9570e17f40234b7
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/dialer/calllog/CallLogQueryHandler.java
+++ src/com/android/dialer/calllog/CallLogQueryHandler.java
@@ -178,6 +178,12 @@ public class CallLogQueryHandler extends NoNullCursorAsyncQueryHandler {
             where.append(String.format("(%s = ?)", Calls.TYPE));
             // Add a clause to fetch only items newer than the requested date
             selectionArgs.add(Integer.toString(callType));
+            if (callType == Calls.MISSED_TYPE) {
+                // also query for blacklisted calls as they are 'missed'
+                where.append(" OR ");
+                where.append(String.format("(%s = ?)", Calls.TYPE));
+                selectionArgs.add(Integer.toString(Calls.BLACKLIST_TYPE));
+            }
         }
 
         if (newerThan > 0) {
@@ -219,6 +225,12 @@ public class CallLogQueryHandler extends NoNullCursorAsyncQueryHandler {
             where.append(String.format("(%s = ?)", Calls.TYPE));
             // Add a clause to fetch only items newer than the requested date
             selectionArgs.add(Integer.toString(callType));
+            if (callType == Calls.MISSED_TYPE) {
+                // also query for blacklisted calls as they are 'missed'
+                where.append(" OR ");
+                where.append(String.format("(%s = ?)", Calls.TYPE));
+                selectionArgs.add(Integer.toString(Calls.BLACKLIST_TYPE));
+            }
         }
 
         if (slotId > CALL_SUB_ALL) {
