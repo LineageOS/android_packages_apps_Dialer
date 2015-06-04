99318f2ac8a17fadb0370275564d39eabb90a102
Anju Mathapati <anjucm@codeaurora.org>
2015-06-04 17:23:29 +0530

    IMS-VT: Update callStateLabel
    Update callStateLabel such that it holds both network name
    and call specific supplimentary service strings
    
    Change-Id: I56532682cdeeb83eed157481909366c2dbf84319
    CRs-Fixed: 848321

--- InCallUI/src/com/android/incallui/CallCardFragment.java
+++ InCallUI/src/com/android/incallui/CallCardFragment.java
@@ -1167,9 +1167,7 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
             case Call.State.ACTIVE:
                 // We normally don't show a "call state label" at all in this state
                 // (but we can use the call state label to display the provider name).
-                if ((isAccount || isWifi || isConference) && hasSuggestedLabel) {
-                    callStateLabel = label;
-                } else if (sessionModificationState
+                if (sessionModificationState
                         == Call.SessionModificationState.REQUEST_REJECTED) {
                     callStateLabel = context.getString(R.string.card_title_video_call_rejected);
                     isAutoDismissing = true;
@@ -1185,6 +1183,11 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
                     callStateLabel = context.getString(R.string.card_title_video_call_requesting);
                 } else if (VideoUtils.isVideoCall(videoState)) {
                     callStateLabel = context.getString(R.string.card_title_video_call);
+                }
+
+                if ((isAccount || isWifi || isConference) && hasSuggestedLabel) {
+                   label += (callStateLabel != null) ? (" " + callStateLabel) : "";
+                   callStateLabel = label;
                 }
                 break;
             case Call.State.ONHOLD:
