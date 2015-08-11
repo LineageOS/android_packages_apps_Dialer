93a6fd8d4a6f3ff4732cb4fb3b9df7c522b92823
Tyler Gunn <tgunn@google.com>
2015-08-11 15:31:01 -0700

    Minimize the number of calls to PhoneNumberUtils.isLocalEmergencyNumber.
    On a standard outgoing call, isLocalEmergencyNumber was being called
    from 30-40 times.  Each of these calls requires a system service
    invocation.
    
    A large number of these invocations were coming from InCall UI.
    
    To fix this:
    1. Added an isEmergencyCall property to the InCall Call object.
    isLocalEmergencyNumber is recalculated any time the mTelecomCall's
    handle changes.
    2. Replaced calls to isLocalEmergencyNumber in CallCardPresenter with
    calls to the InCall call cached version; this easily cuts out around 15
    calls to isLocalEmergencyNumber.
    3. Replaced code in InCallPresenter; this accounted for 2-3 calls to
    isLocalEmergencyNumber.
    
    Bug: 22985202
    Change-Id: Ia61204bb0ed6171b24c116e8c681b3924ce54f1d
    
    # Conflicts:
    #	src/com/android/incallui/Call.java

--- InCallUI/src/com/android/incallui/InCallPresenter.java
+++ InCallUI/src/com/android/incallui/InCallPresenter.java
@@ -1138,7 +1138,7 @@ public class InCallPresenter implements CallList.Listener,
      * call.
      */
     public static boolean isCallWithNoValidAccounts(Call call) {
-        if (call != null && !isEmergencyCall(call)) {
+        if (call != null && !call.isEmergencyCall()) {
             Bundle extras = call.getIntentExtras();
 
             if (extras == null) {
@@ -1155,14 +1155,6 @@ public class InCallPresenter implements CallList.Listener,
             }
         }
         return false;
-    }
-
-    private static boolean isEmergencyCall(Call call) {
-        final Uri handle = call.getHandle();
-        if (handle == null) {
-            return false;
-        }
-        return PhoneNumberUtils.isEmergencyNumber(handle.getSchemeSpecificPart());
     }
 
     /**
