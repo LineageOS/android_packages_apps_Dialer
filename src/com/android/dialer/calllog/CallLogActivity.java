44898ad0aa2a12788fc9cfc6b78fa7adc9d90f23
Nancy Chen <nancychen@google.com>
2015-08-13 12:03:47 -0700

    Convert Dialer activities to AppCompat to support Snackbar.
    In order to add an "Undo" snackbar for voicemail, dialer activities need
    to inherit from the AppCompat library. Add the relevant libraries to the
    make files and change the styles to be compatible.
    
    Bug: 22460745
    Change-Id: Ia3cc4002cbd943da818fec67f888d085d53a3b25

--- src/com/android/dialer/calllog/CallLogActivity.java
+++ src/com/android/dialer/calllog/CallLogActivity.java
@@ -15,7 +15,6 @@
  */
 package com.android.dialer.calllog;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
@@ -27,6 +26,8 @@ import android.provider.CallLog;
 import android.provider.CallLog.Calls;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -42,7 +43,7 @@ import com.android.dialer.R;
 import com.android.dialer.voicemail.VoicemailStatusHelper;
 import com.android.dialer.voicemail.VoicemailStatusHelperImpl;
 
-public class CallLogActivity extends Activity implements ViewPager.OnPageChangeListener {
+public class CallLogActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
     private ViewPager mViewPager;
     private ViewPagerTabs mViewPagerTabs;
     private ViewPagerAdapter mViewPagerAdapter;
@@ -115,7 +116,7 @@ public class CallLogActivity extends Activity implements ViewPager.OnPageChangeL
         setContentView(R.layout.call_log_activity);
         getWindow().setBackgroundDrawable(null);
 
-        final ActionBar actionBar = getActionBar();
+        final ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowTitleEnabled(true);
--- src/com/android/dialer/list/ListsFragment.java
+++ src/com/android/dialer/list/ListsFragment.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.android.dialer.list;
 
 import android.animation.LayoutTransition;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
@@ -14,6 +28,8 @@ import android.provider.CallLog.Calls;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -182,7 +198,7 @@ public class ListsFragment extends Fragment
     public void onResume() {
         Trace.beginSection(TAG + " onResume");
         super.onResume();
-        mActionBar = getActivity().getActionBar();
+        mActionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
         if (getUserVisibleHint()) {
             sendScreenViewForCurrentPosition();
         }
