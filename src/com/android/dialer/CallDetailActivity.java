516be4dc3cb7088aa4344938347e18770f6aff67
Yorke Lee <yorkelee@google.com>
2015-11-09 18:38:51 -0800

    Fix RTL layout for "Block number" action in Call Details
    Use the RTL-adjusted method when assigning the compound
    drawable to the TextView.
    
    Bug: 25513994
    Change-Id: I85103fa92fce6798ba049c6393ca692700099efa

--- src/com/android/dialer/CallDetailActivity.java
+++ src/com/android/dialer/CallDetailActivity.java
@@ -445,11 +445,11 @@ public class CallDetailActivity extends AppCompatActivity
     private void updateBlockActionItem() {
         if (mBlockedNumberId == null) {
             mBlockNumberActionItem.setText(R.string.action_block_number);
-            mBlockNumberActionItem.setCompoundDrawablesWithIntrinsicBounds(
+            mBlockNumberActionItem.setCompoundDrawablesRelativeWithIntrinsicBounds(
                     R.drawable.ic_call_detail_block, 0, 0, 0);
         } else {
             mBlockNumberActionItem.setText(R.string.action_unblock_number);
-            mBlockNumberActionItem.setCompoundDrawablesWithIntrinsicBounds(
+            mBlockNumberActionItem.setCompoundDrawablesRelativeWithIntrinsicBounds(
                     R.drawable.ic_call_detail_unblock, 0, 0, 0);
         }
 
