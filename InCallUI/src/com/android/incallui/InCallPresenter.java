c3b6891511e7912d929e48d41da8a27b0d1315b6
Ravindra <c_rthat@codeaurora.org>
2015-11-12 12:29:27 +0530

    Fix to incall screen for incoming call
    When user answers the call from status bar do not display
    the incall screen until ACTIVE state is received.
    
    Change-Id: I3d9667bccdf1bac3ab817a0282a8140c6dea785b
    CRs-Fixed: 936970

--- InCallUI/src/com/android/incallui/InCallPresenter.java
+++ InCallUI/src/com/android/incallui/InCallPresenter.java
@@ -893,7 +893,6 @@ public class InCallPresenter implements CallList.Listener,
         Call call = mCallList.getIncomingCall();
         if (call != null) {
             TelecomAdapter.getInstance().answerCall(call.getId(), videoState);
-            showInCall(false, false/* newOutgoingCall */);
         }
     }
 
