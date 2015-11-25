beee915e8b18aaa64d65c1115b3a2044ae45e775
Mengjun Leng <mengju@codeaurora.org>
2015-11-25 15:06:57 +0800

    Dialer: Add control for the call duration display
    Add config to display/hide call duration view in call
    
    detail activity by MCC/MNC.
    
    Change-Id: Idfb29c5b8f7d1f27886e08879a89f422dc2ff687
    CRs-Fixed: 943296

--- src/com/android/dialer/calllog/CallDetailHistoryAdapter.java
+++ src/com/android/dialer/calllog/CallDetailHistoryAdapter.java
@@ -118,7 +118,10 @@ public class CallDetailHistoryAdapter extends BaseAdapter {
                 DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_YEAR);
         dateView.setText(dateValue);
         // Set the duration
-        if (Calls.VOICEMAIL_TYPE == callType || CallTypeHelper.isMissedCallType(callType)) {
+        boolean callDurationEnabled = mContext.getResources()
+                .getBoolean(R.bool.call_duration_enabled);
+        if (Calls.VOICEMAIL_TYPE == callType || CallTypeHelper.isMissedCallType(callType) ||
+                !callDurationEnabled) {
             durationView.setVisibility(View.GONE);
         } else {
             durationView.setVisibility(View.VISIBLE);
