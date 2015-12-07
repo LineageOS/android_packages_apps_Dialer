77d71ab22be0dd2075391b1233833397f8694ece
Martin Herndl <martin.herndl@gmail.com>
2015-12-07 18:17:26 +0100

    [WIP] Fix and re-invent "Whitepages"
    The new implementation uses the same regexes on the Canadian and
    American Whitepages sites which makes it better maintainable and
    future-proof. Furthermore there is no more fiddling around with
    Cookies but the "CookieManager" and "CookieHandler" together with
    the "HttpURLConnection" used instead.
    Also removed the redundant declarations of "ContactInfo" by moving
    the ContactBuilder-logic into the API class.
    
    Problems to resolve: Whitepages Cookie-Queries are slow and they
    limit / ask for Captchas or even block completely if we query too
    often or too fast.
    
    Change-Id: Ie80001b4c72e529de5a432316ba2d344722eb097

--- src/com/android/dialer/lookup/whitepages/WhitePagesPeopleLookup.java
+++ src/com/android/dialer/lookup/whitepages/WhitePagesPeopleLookup.java
@@ -17,62 +17,29 @@
 package com.android.dialer.lookup.whitepages;
 
 import com.android.dialer.calllog.ContactInfo;
-import com.android.dialer.lookup.ContactBuilder;
 import com.android.dialer.lookup.PeopleLookup;
 
 import android.content.Context;
-import android.location.Location;
-import android.provider.ContactsContract.CommonDataKinds.Phone;
-import android.provider.ContactsContract.CommonDataKinds.StructuredPostal;
-import android.provider.ContactsContract.CommonDataKinds.Website;
 import android.util.Log;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 public class WhitePagesPeopleLookup extends PeopleLookup {
-    private static final String TAG =
-            WhitePagesPeopleLookup.class.getSimpleName();
+    private static final String TAG = WhitePagesPeopleLookup.class.getSimpleName();
 
     public WhitePagesPeopleLookup(Context context) {
     }
 
     @Override
     public ContactInfo[] lookup(Context context, String filter) {
-        WhitePagesApi.ContactInfo[] infos = null;
-
+        List<ContactInfo> infos = null;
         try {
-            infos = WhitePagesApi.peopleLookup(context, filter, 3);
+            infos = WhitePagesApi.peopleLookup(context, filter);
         } catch (IOException e) {
             Log.e(TAG, "People lookup failed", e);
         }
-
-        if (infos == null || infos.length == 0) {
-            return null;
-        }
-
-        ContactInfo[] details = new ContactInfo[infos.length];
-        for (int i = 0; i < infos.length; i++) {
-            WhitePagesApi.ContactInfo info = infos[i];
-            ContactBuilder builder = new ContactBuilder(
-                    ContactBuilder.PEOPLE_LOOKUP, null, info.formattedNumber);
-
-            builder.setName(ContactBuilder.Name.createDisplayName(info.name));
-            builder.addPhoneNumber(
-                    ContactBuilder.PhoneNumber.createMainNumber(info.formattedNumber));
-            builder.addWebsite(ContactBuilder.WebsiteUrl.createProfile(info.website));
-
-            if (info.address != null || info.city != null) {
-                ContactBuilder.Address a = new ContactBuilder.Address();
-                a.city = info.city;
-                a.formattedAddress = info.address;
-                a.type = StructuredPostal.TYPE_HOME;
-                builder.addAddress(a);
-            }
-
-            details[i] = builder.build();
-        }
-
-        return details;
+        return (infos != null && !infos.isEmpty())
+                ? infos.toArray(new ContactInfo[infos.size()]) : null;
     }
 }
