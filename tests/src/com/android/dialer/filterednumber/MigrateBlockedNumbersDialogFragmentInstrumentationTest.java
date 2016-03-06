c6ed16091cf15c3546d3bd1188e14b8aef935280
Brandon Maxwell <maxwelb@google.com>
2016-03-06 13:52:20 -0800

    Adding block numbers migration dialog
    + When the user attempts to block a number and they haven't migrated
    to the framework blocking implementation, they should be shown a
    dialog that asks them to migrate. This CL introduces the Dialog that
    is shown and updates the Call log and Call details to open it.
    + As part one of the change, the Dialog is shown every time the user
    attempts to block or unblock a number (when the feature is enabled).
    A later CL will complete this migration step to ensure that the
    dialog is only shown until migration is finished.
    
    Bug: 26664600
    Change-Id: Ia4c2d504f8d98679b90d232058eb5ee6ea9b38f1

---
+++ tests/src/com/android/dialer/filterednumber/MigrateBlockedNumbersDialogFragmentInstrumentationTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.dialer.filterednumber;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.android.dialer.DialtactsActivity;
+import com.android.dialer.filterednumber.MigrateBlockedNumbersDialogFragment.Listener;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Instrumentation tests for {@link MigrateBlockedNumbersDialogFragment}. Note for these tests to
+ * work properly, the device's screen must be on.
+ */
+public class MigrateBlockedNumbersDialogFragmentInstrumentationTest extends
+        ActivityInstrumentationTestCase2<DialtactsActivity> {
+
+    private static final String SHOW_TAG = "ShowTag";
+
+    @Mock private Listener mListener;
+    private DialtactsActivity mActivity;
+    private DialogFragment mMigrateDialogFragment;
+
+    public MigrateBlockedNumbersDialogFragmentInstrumentationTest() {
+        super(DialtactsActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        MockitoAnnotations.initMocks(this);
+        mActivity = getActivity();
+        mMigrateDialogFragment = MigrateBlockedNumbersDialogFragment.newInstance(mListener);
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                mMigrateDialogFragment.show(mActivity.getFragmentManager(), SHOW_TAG);
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+    }
+
+    public void testDialogAppears() {
+        assertTrue(mMigrateDialogFragment.getDialog().isShowing());
+    }
+
+    public void testDialogPositiveButtonPress() {
+        doNothing().when(mListener).onComplete();
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                ((AlertDialog) mMigrateDialogFragment.getDialog())
+                        .getButton(DialogInterface.BUTTON_POSITIVE).performClick();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        // Dialog was dismissed
+        assertNull(mMigrateDialogFragment.getDialog());
+        verify(mListener).onComplete();
+    }
+}
