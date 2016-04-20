304a2c61de083b8b40b69fd7c2f738f81cad1148
NathanielWaggoner <nwaggoner@cyngn.com>
2016-04-19 17:05:06 -0700

    Dialer DeepLink Integration
    Change-Id: I90fe73dff9615a9955e61700fe9a712e6a5952ed

--- src/com/android/dialer/calllog/CallLogListItemViewHolder.java
+++ src/com/android/dialer/calllog/CallLogListItemViewHolder.java
@@ -57,8 +57,11 @@ import com.android.dialer.voicemail.VoicemailPlaybackLayout;
 
 import com.android.phone.common.incall.CallMethodHelper;
 import com.android.phone.common.incall.CallMethodInfo;
-import com.cyanogen.ambient.incall.extension.OriginCodes;
+
 import com.cyanogen.lookup.phonenumber.provider.LookupProviderImpl;
+
+import com.cyanogen.ambient.incall.extension.OriginCodes;
+import com.cyanogen.ambient.deeplink.DeepLink;
 
 /**
  * This is an object containing references to views contained by the call log list item. This
@@ -86,6 +89,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
     public final ImageView primaryActionButtonView;
     /** DialerQuickContact */
     public final DialerQuickContact dialerQuickContact;
+    public DeepLink mDeepLink;
 
     /** The view containing call log item actions.  Null until the ViewStub is inflated. */
     public View actionsView;
@@ -93,6 +97,7 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
     public VoicemailPlaybackLayout voicemailPlaybackView;
     public View callButtonView;
     public View videoCallButtonView;
+    public View viewNoteButton;
     public View createNewContactButtonView;
     public View addToExistingContactButtonView;
     public View sendMessageView;
@@ -116,6 +121,11 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
      */
     public long[] callIds;
 
+    /**
+     * The time for each call in callIds represented by the current call log entry.  Used when the
+     * user views the call log to determine note status
+     */
+    public long[] callTimes;
     /**
      * The callable phone number for the current call log entry.  Cached here as the call back
      * intent is set only when the actions ViewStub is inflated.
@@ -298,6 +308,9 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
             createNewContactButtonView = actionsView.findViewById(R.id.create_new_contact_action);
             createNewContactButtonView.setOnClickListener(this);
 
+            viewNoteButton = actionsView.findViewById(R.id.view_note_action);
+            viewNoteButton.setOnClickListener(this);
+
             addToExistingContactButtonView =
                     actionsView.findViewById(R.id.add_to_existing_contact_action);
             addToExistingContactButtonView.setOnClickListener(this);
@@ -387,6 +400,12 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
             callButtonView.setVisibility(View.GONE);
         }
 
+        if(mDeepLink!=null) {
+            ImageView icon = (ImageView) viewNoteButton.findViewById(R.id.view_note_action_icon);
+            icon.setImageDrawable(mDeepLink.getDrawableIcon(mContext));
+        } else {
+            viewNoteButton.setVisibility(View.GONE);
+        }
         // If one of the calls had video capabilities, show the video call button.
         if (mTelecomCallLogCache.isVideoEnabled() && canPlaceCallToNumber &&
                 phoneCallDetailsViews.callTypeIcons.isVideoShown() ||
@@ -564,6 +583,9 @@ public final class CallLogListItemViewHolder extends RecyclerView.ViewHolder
                                                                            view in dialog. */
                     numberType, /* phone number type (e.g. mobile) in second line of contact view */
                     accountHandle);
+
+        } else if (view.getId() == R.id.view_note_action){
+            mContext.startActivity(mDeepLink.createViewIntent());
         } else {
             final String inCallAction = (String) view.getTag(R.id.incall_provider_action_type);
             if (inCallComponentName != null && !TextUtils.isEmpty(inCallAction)) {
