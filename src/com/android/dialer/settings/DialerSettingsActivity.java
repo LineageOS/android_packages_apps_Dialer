50c3a511aabc3f24161f9e61c6b06c0c3a8b6d12
NathanielWaggoner <nwaggoner@cyngn.com>
2016-05-09 11:36:18 -0700

    Fixes an issue where Note settings weren't launching when clicked.
    A refactor patch broke note settings, this gets them working again.
    
    NOTES-109
    
    Change-Id: Ib8c2be7cb87811af973594d27007e25a5daca73d

--- src/com/android/dialer/settings/DialerSettingsActivity.java
+++ src/com/android/dialer/settings/DialerSettingsActivity.java
@@ -302,6 +302,7 @@ public class DialerSettingsActivity extends PreferenceActivity {
             Header noteHeader = new Header();
             noteHeader.title = mDeepLinkPluginInfo.get(0);
             noteHeader.summaryRes = R.string.note_mod_settings_summary;
+            noteHeader.id = R.id.note_preference_id;
             target.add(noteHeader);
         }
 
@@ -585,6 +586,9 @@ public class DialerSettingsActivity extends PreferenceActivity {
                                 }
                             }
                         });
+                    } else if (header.id == R.id.note_preference_id){
+                        holder.header = header;
+                        view.setOnClickListener(this);
                     }
                     break;
             }
