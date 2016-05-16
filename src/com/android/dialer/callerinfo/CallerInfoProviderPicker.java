058473c7cea9a08c3873adbf9614a9983ec4abbe
Rohit Yengisetty <rohit@cyngn.com>
2016-05-16 11:28:02 -0700

    Constrain logo bounds within the picker for caller-info provider
    Change-Id: I9017f6bd9a3b28e4c60d67acd030960e61f9be89
    Issue-Id: CYNGNOS-2844
    (cherry picked from commit 9fb1a7b0e02751d0b08be888ef9f9f42d10e9de7)

--- src/com/android/dialer/callerinfo/CallerInfoProviderPicker.java
+++ src/com/android/dialer/callerinfo/CallerInfoProviderPicker.java
@@ -144,8 +144,12 @@ public class CallerInfoProviderPicker {
             subText = Html.fromHtml(text);
         }
 
+        int logoWidth = context.getResources().getDimensionPixelSize(
+                R.dimen.callerinfo_provider_picker_logo_width);
+        int logoHeight = context.getResources().getDimensionPixelSize(
+                R.dimen.callerinfo_provider_picker_logo_height);
         Bitmap logo = ImageUtils.drawableToBitmap(info.getBrandLogo());
-
+        Bitmap scaledLogo = ImageUtils.scaleBitmapToTarget(logo, logoHeight, logoWidth);
         int resId = info.hasProperty(ProviderInfo.PROPERTY_SUPPORTS_SPAM) ?
                 R.string.callerinfo_provider_auth_desc :
                 R.string.callerinfo_provider_auth_desc_no_spam;
@@ -158,8 +162,8 @@ public class CallerInfoProviderPicker {
             nudge.setSubhead(subText.toString());
         }
 
-        if (logo != null) {
-            nudge.setTitleImage(logo);
+        if (scaledLogo != null) {
+            nudge.setTitleImage(scaledLogo);
         }
 
         Intent enableIntent = buildEnableIntent(context, component, info, metricsReason);
