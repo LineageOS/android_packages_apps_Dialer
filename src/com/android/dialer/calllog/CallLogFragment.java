b85d6ad8b707af5b534600baff0226f7f0e1fdff
Rohit Yengisetty <rohit@cyngn.com>
2016-05-16 18:22:43 -0700

    Clear and restart cache on contact-info provider updates
    Change-Id: Ifb4f6ce47580e96024fa199a50d76b92cc9e0376
    Issue-Id: OPO-709
    (cherry picked from commit eb350279ae1e9c3bc03d8d1895ab3493513153d3)

--- src/com/android/dialer/calllog/CallLogFragment.java
+++ src/com/android/dialer/calllog/CallLogFragment.java
@@ -443,8 +443,10 @@ public class CallLogFragment extends Fragment implements CallLogQueryHandler.Lis
 
     @Override
     public void onStatusChanged(boolean enabled) {
-        mRefreshDataRequired = true;
-        refreshData();
+        // reset and start the ContactInfo cache
+        mAdapter.invalidateCache();
+        mAdapter.startCache();
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
