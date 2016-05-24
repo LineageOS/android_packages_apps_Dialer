4ea75d8321df81cd8325b2db3575e1a061eb32cc
NathanielWaggoner <nwaggoner@cyngn.com>
2016-05-24 11:20:29 -0700

    Performance tweaks for DeepLinkCache and icon retrieval.
    We are now caching icons to avoid hitting package manager on each
    UI update.  Also upped cache size to accomodate a larger number
    of DeepLinks smoothly.
    
    Change-Id: I6cfc9c2ff642ed129d619eb2861f32b4027e8367

--- src/com/android/dialer/deeplink/DeepLinkCache.java
+++ src/com/android/dialer/deeplink/DeepLinkCache.java
@@ -19,7 +19,6 @@ package com.android.dialer.deeplink;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
-
 import com.android.dialer.util.ExpirableCache;
 
 import com.android.dialer.deeplink.DeepLinkIntegrationManager;
@@ -59,9 +58,10 @@ public class DeepLinkCache {
 
     }
 
+    private static int DONT_OVERLOAD_AMBIENT = 200;
     private static final int START_THREAD = 0;
     private static final int REDRAW = 1;
-    private static final int DEEP_LINK_CACHE_SIZE = 100;
+    private static final int DEEP_LINK_CACHE_SIZE = 1000;
     private static final int START_PROCESSING_REQUESTS_DELAY_MS = 1000;
     private static final int PROCESSING_THREAD_THROTTLE_LIMIT = 1000;
     private DeepLinkListener mDeepLinkListener;
@@ -117,8 +117,9 @@ public class DeepLinkCache {
                 // Obtain next request, if any is available.
                 // Keep synchronized section small.
                 DeepLinkRequest req = null;
+                int pendingSize = mPendingRequests.size();
                 synchronized (mRequests) {
-                    if (!mRequests.isEmpty()) {
+                    if (!mRequests.isEmpty() && pendingSize < DONT_OVERLOAD_AMBIENT) {
                         req = mRequests.removeFirst();
                     }
                 }
@@ -128,7 +129,7 @@ public class DeepLinkCache {
                 } else if (needRedraw) {
                     needRedraw = false;
                     mHandler.sendEmptyMessage(REDRAW);
-                } else {
+                } else if (mPendingRequests.size() < DONT_OVERLOAD_AMBIENT){
                     // Wait until another request is available, or until this
                     // thread is no longer needed (as indicated by being
                     // interrupted).
@@ -170,7 +171,7 @@ public class DeepLinkCache {
             DeepLink info = cachedInfo == null ? null : cachedInfo.getValue();
             if (cachedInfo == null) {
                 // if its null we need to add a uri to our requests
-                mCache.put(uriString, DeepLinkRequest.EMPTY);
+                //mCache.put(uriString, DeepLinkRequest.EMPTY);
                 urisToRequest.add(uri);
                 // if we get any uris that haven't been handled we need to immediately do this query
                 immediate = true;
@@ -182,7 +183,7 @@ public class DeepLinkCache {
                 toReturn = info;
             }
         }
-        // issue new requests for any uri's we haven't handled previously
+        // issue new requests for any uri's we haven't handleg previously
         if (urisToRequest.size() > 0) {
             enqueueRequest(urisToRequest, immediate);
         }
@@ -227,8 +228,17 @@ public class DeepLinkCache {
         if (mDeepLinkQueryThread != null) {
             // Stop the thread; we are finished with it.
             mDeepLinkQueryThread.stopProcessing();
+            cancelAllPendingQueries();
             mDeepLinkQueryThread = null;
             mRequests.clear();
+        }
+    }
+
+    private void cancelAllPendingQueries() {
+        synchronized (mPendingRequests) {
+            for (PendingResult<DeepLink.DeepLinkResultList> r : mPendingRequests.values()) {
+                r.cancel();
+            }
             mPendingRequests.clear();
         }
     }
@@ -269,7 +279,7 @@ public class DeepLinkCache {
                 if (mDeepLinkQueryThread != null) {
                     mDeepLinkQueryThread.needRedraw = true;
                 }
-            }
+            } 
         }
     }
 
@@ -324,4 +334,5 @@ public class DeepLinkCache {
     public void clearCache() {
         mCache.clearCache();
     }
+
 }
