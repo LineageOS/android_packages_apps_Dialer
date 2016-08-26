854224e9d1aaee8bb9a8c065c860012e997d7be0
Omkar Kolangade <omkark@codeaurora.org>
2016-08-25 20:51:10 -0700

    Revert "Expedite the processing of destroying Incall UI after end call"
    This reverts commit 76876caffbb40f4929d62459152a785698ac1540.
    
    This change is causing regression in basic call end scenarios.
    
    Change-Id: I97f6ecc92f3fa816874540664c11ecb66cc39338
    CRs-Fixed: 1058473

--- InCallUI/src/com/android/incallui/CallList.java
+++ InCallUI/src/com/android/incallui/CallList.java
@@ -538,8 +538,7 @@ public class CallList {
 
         boolean updated = false;
 
-        if (call.getState() == Call.State.DISCONNECTING
-                || call.getState() == Call.State.DISCONNECTED) {
+        if (call.getState() == Call.State.DISCONNECTED) {
             // update existing (but do not add!!) disconnected calls
             if (mCallById.containsKey(call.getId())) {
                 // For disconnected calls, we want to keep them alive for a few seconds so that the
@@ -568,8 +567,8 @@ public class CallList {
     }
 
     private int getDelayForDisconnect(Call call) {
-        Preconditions.checkState(call.getState() == Call.State.DISCONNECTED
-                || call.getState() == Call.State.DISCONNECTING);
+        Preconditions.checkState(call.getState() == Call.State.DISCONNECTED);
+
 
         final int cause = call.getDisconnectCause().getCode();
         final int delay;
@@ -584,7 +583,6 @@ public class CallList {
             case DisconnectCause.REJECTED:
             case DisconnectCause.MISSED:
             case DisconnectCause.CANCELED:
-            case DisconnectCause.UNKNOWN:
                 // no delay for missed/rejected incoming calls and canceled outgoing calls.
                 delay = 0;
                 break;
