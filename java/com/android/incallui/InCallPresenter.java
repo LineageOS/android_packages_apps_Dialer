f627984ac8de591d916076c65de5fb95c7587894
Danny Baumann <dannybaumann@web.de>
2016-09-09 10:01:27 +0200

    Add back in-call vibration features
    Change-Id: Ia30a3c46e9c37d7d73288ec605af8efc3a46a323

--- java/com/android/incallui/InCallPresenter.java
+++ java/com/android/incallui/InCallPresenter.java
@@ -117,6 +117,7 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
 
   private StatusBarNotifier statusBarNotifier;
   private ExternalCallNotifier externalCallNotifier;
+  private InCallVibrationHandler vibrationHandler;
   private ContactInfoCache contactInfoCache;
   private Context context;
   private final OnCheckBlockedListener onCheckBlockedListener =
@@ -348,6 +349,9 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
     EnrichedCallComponent.get(this.context)
         .getEnrichedCallManager()
         .registerStateChangedListener(this.statusBarNotifier);
+
+    vibrationHandler = new InCallVibrationHandler(context);
+    addListener(vibrationHandler);
 
     this.proximitySensor = proximitySensor;
     addListener(this.proximitySensor);
@@ -1466,6 +1470,11 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
         externalCallList.removeExternalCallListener(externalCallNotifier);
       }
       statusBarNotifier = null;
+
+      if (vibrationHandler != null) {
+        removeListener(vibrationHandler);
+      }
+      vibrationHandler = null;
 
       if (callList != null) {
         callList.removeListener(this);
