12bbdb5ade40182c3f80e5df112045710ff7ae5a
Danny Baumann <dannybaumann@web.de>
2016-09-14 15:27:41 +0200

    Re-add call stats feature.
    Change-Id: Ib00ab753077788670f1d3390b2031beecb3b65c4

--- src/com/android/dialer/calllog/CallLogActivity.java
+++ src/com/android/dialer/calllog/CallLogActivity.java
@@ -57,17 +57,21 @@ import com.android.contacts.commonbind.analytics.AnalyticsUtil;
 import com.android.dialer.DialtactsActivity;
 import com.android.dialer.R;
 import com.android.dialer.TransactionSafeActivity;
+import com.android.dialer.callstats.CallStatsFragment;
 import com.android.dialer.logging.Logger;
 import com.android.dialer.logging.ScreenEvent;
 import com.android.dialer.util.DialerUtils;
+import com.android.dialer.widget.DoubleDatePickerDialog;
 
-public class CallLogActivity extends TransactionSafeActivity implements ViewPager.OnPageChangeListener,
-    CallLogFragment.HostInterface {
+public class CallLogActivity extends TransactionSafeActivity implements
+        ViewPager.OnPageChangeListener, CallLogFragment.HostInterface,
+        DoubleDatePickerDialog.OnDateSetListener {
     private ViewPager mViewPager;
     private ViewPagerTabs mViewPagerTabs;
     private FragmentPagerAdapter mViewPagerAdapter;
     private CallLogFragment mAllCallsFragment;
     private CallLogFragment mMissedCallsFragment;
+    private CallStatsFragment mStatsFragment;
 
     private MSimCallLogFragment mMSimCallsFragment;
     private CallLogSearchFragment mSearchFragment;
@@ -78,8 +82,9 @@ public class CallLogActivity extends TransactionSafeActivity implements ViewPage
     private String mSearchQuery;
     private static final int TAB_INDEX_ALL = 0;
     private static final int TAB_INDEX_MISSED = 1;
+    private static final int TAB_INDEX_STATS = 2;
 
-    private static final int TAB_INDEX_COUNT = 2;
+    private static final int TAB_INDEX_COUNT = 3;
 
     private boolean mIsResumed;
 
@@ -104,20 +109,24 @@ public class CallLogActivity extends TransactionSafeActivity implements ViewPage
                             CallLogQueryHandler.CALL_TYPE_ALL, true /* isCallLogActivity */);
                 case TAB_INDEX_MISSED:
                     return new CallLogFragment(Calls.MISSED_TYPE, true /* isCallLogActivity */);
+                case TAB_INDEX_STATS:
+                    return new CallStatsFragment();
             }
             throw new IllegalStateException("No fragment at position " + position);
         }
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            final CallLogFragment fragment =
-                    (CallLogFragment) super.instantiateItem(container, position);
-            switch (position) {
+            final Object fragment = super.instantiateItem(container, position);
+            switch (getRtlPosition(position)) {
                 case TAB_INDEX_ALL:
-                    mAllCallsFragment = fragment;
+                    mAllCallsFragment = (CallLogFragment) fragment;
                     break;
                 case TAB_INDEX_MISSED:
-                    mMissedCallsFragment = fragment;
+                    mMissedCallsFragment = (CallLogFragment) fragment;
+                    break;
+                case TAB_INDEX_STATS:
+                    mStatsFragment = (CallStatsFragment) fragment;
                     break;
             }
             return fragment;
@@ -193,6 +202,7 @@ public class CallLogActivity extends TransactionSafeActivity implements ViewPage
         mTabTitles = new String[TAB_INDEX_COUNT];
         mTabTitles[0] = getString(R.string.call_log_all_title);
         mTabTitles[1] = getString(R.string.call_log_missed_title);
+        mTabTitles[2] = getString(R.string.call_log_stats_title);
 
         mViewPager = (ViewPager) findViewById(R.id.call_log_pager);
 
@@ -329,6 +339,15 @@ public class CallLogActivity extends TransactionSafeActivity implements ViewPage
     @Override
     public void onPageScrollStateChanged(int state) {
         mViewPagerTabs.onPageScrollStateChanged(state);
+    }
+
+    @Override
+    public void onDateSet(long from, long to) {
+        switch (mViewPager.getCurrentItem()) {
+            case TAB_INDEX_STATS:
+                mStatsFragment.onDateSet(from, to);
+                break;
+        }
     }
 
     private void sendScreenViewForChildFragment(int position) {
