d9859a7eb008196ba91073645567ab9212a82f17
Eric Erfanian <erfanian@google.com>
2017-06-21 09:40:17 -0700

    Dismiss popup menu when show dialpad chooser.
    Issue steps:
    1. Show overflow menu in dialpad.
    2. Press Home button, start another app such as Messaging.
    3. Make phone call by Messaging app.
    4. After the call is active, press home button and start Dialer again.
    5. Dialpad is dismissed but popup overflow menu still shows.
    
    Solution:
    Dismiss popup menu when show dialpad chooser.
    
    Credit to:
    yifan.bai <yifan.bai@spreadtrum.com>
    https://android-review.googlesource.com/#/c/417719/
    
    Test: TH, manual
    Change-Id: I019a799d52282bfb47a09d43cf25c14b053f8d9b

--- java/com/android/dialer/app/dialpad/DialpadFragment.java
+++ java/com/android/dialer/app/dialpad/DialpadFragment.java
@@ -1176,6 +1176,10 @@ public class DialpadFragment extends Fragment
         mDialpadView.setVisibility(View.GONE);
       }
 
+      if (mOverflowPopupMenu != null) {
+        mOverflowPopupMenu.dismiss();
+      }
+
       mFloatingActionButtonController.setVisible(false);
       mDialpadChooser.setVisibility(View.VISIBLE);
 
