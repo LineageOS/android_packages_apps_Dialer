736d0db37191b0c30e7c4f88fe495ab1e3de913a
roldenburg <roldenburg@google.com>
2017-07-25 16:27:15 -0700

    Filter out Duo audio calls
    Without this CL, Duo audio calls show in the Dialer call log and will either
    start phone calls or Duo video calls based on Duo integration being available
    or not. Carriers do not want OTT audio calling in the Dialer call log so we
    should filter the audio calls out.
    
    Bug: 63089358
    Test: Manual: Placed duo audio and video calls, verified they showed without the patch, verified correct calls were filtered out with it. Will want QA verification over other test scenarios including multi-sim if we were to put this into OC-DR.
    PiperOrigin-RevId: 163140580
    Change-Id: I83c3659f6c356522b62d9ced2002a808ea958c95

--- java/com/android/dialer/database/CallLogQueryHandler.java
+++ java/com/android/dialer/database/CallLogQueryHandler.java
@@ -190,6 +190,18 @@ public class CallLogQueryHandler extends NoNullCursorAsyncQueryHandler {
           .appendOmtpVoicemailSelectionClause(mContext, where, selectionArgs);
     }
 
+    // Filter out all Duo entries other than video calls
+    where
+        .append(" AND (")
+        .append(Calls.PHONE_ACCOUNT_COMPONENT_NAME)
+        .append(" NOT LIKE 'com.google.android.apps.tachyon%' OR ")
+        .append(Calls.FEATURES)
+        .append(" & ")
+        .append(Calls.FEATURES_VIDEO)
+        .append(" == ")
+        .append(Calls.FEATURES_VIDEO)
+        .append(")");
+
     final int limit = (mLogLimit == -1) ? NUM_LOGS_TO_DISPLAY : mLogLimit;
     final String selection = where.length() > 0 ? where.toString() : null;
     Uri uri =
