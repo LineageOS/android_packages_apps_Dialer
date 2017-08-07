39eeda8cf081e9f5335955dd7d6543041925e22e
erfanian <erfanian@google.com>
2017-08-07 14:22:36 -0700

    Move TelphonyManagerCompat outside of contacts/common.
    This class will be used for Assisted Dialing.
    
    #turndowncontactscommon
    
    Bug: 64205446,37208802
    Test: TAP
    PiperOrigin-RevId: 164510740
    Change-Id: I5dec67d2182b33bf2057953aab69e3b561af5708

--- java/com/android/dialer/compat/telephony/TelephonyManagerCompat.java
+++ java/com/android/dialer/compat/telephony/TelephonyManagerCompat.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.contacts.common.compat;
+package com.android.dialer.compat.telephony;
 
 import android.content.Context;
 import android.content.Intent;
