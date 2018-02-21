82133d92307bc26327be7609262e4684df3af669
twyen <twyen@google.com>
2018-02-20 23:47:14 -0800

    Rebuild Call Log on first launch after NUI is enabled
    This improves the first time experience by populating the DB when the user is still in other tabs.
    
    Bug: 72119926
    Test: Unit tests
    PiperOrigin-RevId: 186418788
    Change-Id: If011d7191a09fd1aaca489c6e682ccdc643c2139

--- java/com/android/dialer/calllog/CallLogComponent.java
+++ java/com/android/dialer/calllog/CallLogComponent.java
@@ -30,6 +30,8 @@ public abstract class CallLogComponent {
 
   public abstract RefreshAnnotatedCallLogWorker getRefreshAnnotatedCallLogWorker();
 
+  public abstract AnnotatedCallLogMigrator getAnnotatedCallLogMigrator();
+
   public abstract ClearMissedCalls getClearMissedCalls();
 
   public static CallLogComponent get(Context context) {
