f3a57463bfc454135528fa9de26b0855678266ff
blunden <blunden2@gmail.com>
2018-03-18 14:02:30 +0100

    Add setting to enable Do Not Disturb during calls
    Android N and earlier enabled Do Not Disturb mode while in a call.
    Reimplement this behavior to prevent incoming notifications from
    vibrating or playing sounds.
    
    Change-Id: Ic38ac775c6e353898190e1571b6d6e521c0e5aa1

--- java/com/android/dialer/app/settings/SoundSettingsFragment.java
+++ java/com/android/dialer/app/settings/SoundSettingsFragment.java
@@ -16,7 +16,11 @@
 
 package com.android.dialer.app.settings;
 
+import android.app.AlertDialog;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.RingtoneManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -71,6 +75,9 @@ public class SoundSettingsFragment extends PreferenceFragment
   private SwitchPreference mVibrateWhenRinging;
   private SwitchPreference mPlayDtmfTone;
   private ListPreference mDtmfToneLength;
+  private SwitchPreference mEnableDndInCall;
+
+  private NotificationManager mNotificationManager;
 
   @Override
   public Context getContext() {
@@ -93,6 +100,7 @@ public class SoundSettingsFragment extends PreferenceFragment
     mDtmfToneLength =
         (ListPreference)
             findPreference(context.getString(R.string.dtmf_tone_length_preference_key));
+    mEnableDndInCall = (SwitchPreference) findPreference("incall_enable_dnd");
 
     if (hasVibrator()) {
       mVibrateWhenRinging.setOnPreferenceChangeListener(this);
@@ -117,6 +125,8 @@ public class SoundSettingsFragment extends PreferenceFragment
     mPlayDtmfTone.setOnPreferenceChangeListener(this);
     mPlayDtmfTone.setChecked(shouldPlayDtmfTone());
 
+    mEnableDndInCall.setOnPreferenceChangeListener(this);
+
     TelephonyManager telephonyManager =
         (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
     if (SdkVersionOverride.getSdkVersion(Build.VERSION_CODES.M) >= Build.VERSION_CODES.M
@@ -132,6 +142,7 @@ public class SoundSettingsFragment extends PreferenceFragment
       getPreferenceScreen().removePreference(mDtmfToneLength);
       mDtmfToneLength = null;
     }
+    mNotificationManager = context.getSystemService(NotificationManager.class);
   }
 
   @Override
@@ -180,6 +191,37 @@ public class SoundSettingsFragment extends PreferenceFragment
       int index = mDtmfToneLength.findIndexOfValue((String) objValue);
       Settings.System.putInt(
           getActivity().getContentResolver(), Settings.System.DTMF_TONE_TYPE_WHEN_DIALING, index);
+    }
+    if (preference == mEnableDndInCall) {
+      boolean newValue = (Boolean) objValue;
+      if (newValue) {
+        if (mNotificationManager.isNotificationPolicyAccessGranted()) {
+          return true;
+        } else {
+          AlertDialog dialog = new AlertDialog.Builder(getContext())
+              .setMessage(R.string.incall_dnd_dialog_message)
+              .setPositiveButton(R.string.allow, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                  dialog.dismiss();
+                  Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                  startActivity(intent);
+                }
+              })
+              .setNegativeButton(R.string.deny, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                  dialog.dismiss();
+                }
+              })
+              .create();
+          dialog.show();
+
+          // At this time, it is unknown whether the user granted the permission
+          return false;
+        }
+      }
+      return true;
     }
     return true;
   }
