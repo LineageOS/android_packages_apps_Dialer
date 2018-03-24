cac4acd257f66c2639c4daa26be72484d9865ef6
Weijia Xu <weijiaxu@google.com>
2018-03-23 18:13:41 -0700

    Add RootComponentGenerator to auto-generate root components for different dialer variants.
    Bug: 35612086
    Test: Test included
    PiperOrigin-RevId: 189981890
    Change-Id: Ife99969189d5d37bb1ad8ba61361a51e78abdfbb

---
+++ java/com/android/dialer/rootcomponentgenerator/processor/MetadataGeneratingStep.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.dialer.rootcomponentgenerator.processor;
+
+import static javax.tools.Diagnostic.Kind.ERROR;
+
+import com.android.dialer.rootcomponentgenerator.annotation.DialerComponent;
+import com.android.dialer.rootcomponentgenerator.annotation.InstallIn;
+import com.android.dialer.rootcomponentgenerator.annotation.RootComponentGeneratorMetadata;
+import com.google.auto.common.BasicAnnotationProcessor.ProcessingStep;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.SetMultimap;
+import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.TypeSpec;
+import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.Set;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
+
+/**
+ * Genereates metadata for every type annotated by {@link InstallIn} and {@link DialerComponent}.
+ *
+ * <p>The metadata has the information where the annotated types are and it is used by annotation
+ * processor when the processor tries to generate root component.
+ */
+final class MetadataGeneratingStep implements ProcessingStep {
+
+  private final ProcessingEnvironment processingEnv;
+
+  MetadataGeneratingStep(ProcessingEnvironment processingEnv) {
+    this.processingEnv = processingEnv;
+  }
+
+  @Override
+  public Set<? extends Class<? extends Annotation>> annotations() {
+    return ImmutableSet.of(DialerComponent.class, InstallIn.class);
+  }
+
+  @Override
+  public Set<? extends Element> process(
+      SetMultimap<Class<? extends Annotation>, Element> elementsByAnnotation) {
+
+    for (Element element : elementsByAnnotation.get(DialerComponent.class)) {
+      generateMetadataFor(DialerComponent.class, element);
+    }
+    for (Element element : elementsByAnnotation.get(InstallIn.class)) {
+      if (element.getAnnotation(InstallIn.class).variants().length == 0) {
+        processingEnv
+            .getMessager()
+            .printMessage(
+                ERROR, String.format("@InstallIn %s must have at least one variant", element));
+        continue;
+      }
+      generateMetadataFor(InstallIn.class, element);
+    }
+
+    return Collections.emptySet();
+  }
+
+  private void generateMetadataFor(
+      Class<? extends Annotation> annotation, Element annotatedElement) {
+    TypeSpec.Builder metadataClassBuilder =
+        TypeSpec.classBuilder(annotatedElement.getSimpleName() + "Metadata");
+    metadataClassBuilder.addAnnotation(
+        AnnotationSpec.builder(RootComponentGeneratorMetadata.class)
+            .addMember("tag", "$S", annotation.getSimpleName())
+            .addMember("annotatedClass", "$T.class", annotatedElement.asType())
+            .build());
+    TypeSpec metadataClass = metadataClassBuilder.build();
+    RootComponentUtils.writeJavaFile(
+        processingEnv, RootComponentUtils.METADATA_PACKAGE_NAME, metadataClass);
+  }
+}
