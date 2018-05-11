ef5c101361fc4cf23c12aa7ef24d027155b7554a
wangqi <wangqi@google.com>
2018-05-11 11:40:52 -0700

    Use old BottomSheetBehavior.
    Test: presubmit
    PiperOrigin-RevId: 196285108
    Change-Id: Ie424d231ced4066b0d5b734bebae443942994943

--- java/com/android/dialer/main/impl/OldMainActivityPeer.java
+++ java/com/android/dialer/main/impl/OldMainActivityPeer.java
@@ -36,7 +36,7 @@ import android.provider.ContactsContract.QuickContact;
 import android.provider.VoicemailContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.bottomsheet.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
