37d4f326075724a3a5706774962c650b456b7e11
Vasyl Gello <vasek.gello@gmail.com>
2018-06-02 16:44:13 +0300

    InCallUI: Refactor primary call info card
    The primary call info card was sized after the CallButtonFragment
    making the call info displayed small. This patch fixes the sizing
    
    Change-Id: If5dded59960c2d4534d3d7377b6970e17977d33c

--- InCallUI/src/com/android/incallui/CallCardFragment.java
+++ InCallUI/src/com/android/incallui/CallCardFragment.java
@@ -653,10 +653,8 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
 
         if (TextUtils.isEmpty(number) && TextUtils.isEmpty(label)) {
             mCallNumberAndLabel.setVisibility(View.GONE);
-            mElapsedTime.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
         } else {
             mCallNumberAndLabel.setVisibility(View.VISIBLE);
-            mElapsedTime.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
         }
 
         setPrimaryPhoneNumber(number);
