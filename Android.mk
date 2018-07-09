b90bc9abc3b83a207c962857c043faccd9b88eea
Danny Baumann <dannybaumann@web.de>
2018-07-09 11:19:24 +0200

    Re-add call recording.
    Change-Id: I53fadf5754b5b6cc3e9920d57480e470e2305ac0

--- Android.mk
+++ Android.mk
@@ -79,7 +79,7 @@ LOCAL_SRC_FILES += $(call all-proto-files-under, $(BASE_DIR))
 LOCAL_SRC_FILES += $(call all-Iaidl-files-under, $(BASE_DIR))
 LOCAL_SRC_FILES := $(filter-out $(EXCLUDE_FILES),$(LOCAL_SRC_FILES))
 
-LOCAL_AIDL_INCLUDES := $(call all-Iaidl-files-under, $(BASE_DIR))
+LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/java
 
 LOCAL_PROTOC_FLAGS := --proto_path=$(LOCAL_PATH)
 
