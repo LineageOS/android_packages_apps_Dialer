4ca9e8da3d5c1469e9b203874c9c4e7d4db3eb92
Eric Erfanian <erfanian@google.com>
2018-07-10 18:37:03 +0000

    Revert "UI refresh for ringing screen."
    This reverts commit 36d5b93da4f93e50deb496daa5ad820c48aa7b01.
    
    Change-Id: I533bc3f7d592a6e257d184f0d3730b4be95f2af2

--- java/com/android/incallui/answer/impl/AnswerFragment.java
+++ java/com/android/incallui/answer/impl/AnswerFragment.java
@@ -880,6 +880,8 @@ public class AnswerFragment extends Fragment
     if (primaryCallState != null) {
       contactGridManager.setCallState(primaryCallState);
     }
+
+    restoreBackgroundMaskColor();
   }
 
   @Override
@@ -898,6 +900,12 @@ public class AnswerFragment extends Fragment
 
   @Override
   public void onAnswerProgressUpdate(@FloatRange(from = -1f, to = 1f) float answerProgress) {
+    // Don't fade the window background for call waiting or video upgrades. Fading the background
+    // shows the system wallpaper which looks bad because on reject we switch to another call.
+    if (primaryCallState.state() == DialerCallState.INCOMING && !isVideoCall()) {
+      answerScreenDelegate.updateWindowBackgroundColor(answerProgress);
+    }
+
     // Fade and scale contact name and video call text
     float startDelay = .25f;
     // Header progress is zero over positiveAdjustedProgress = [0, startDelay],
@@ -926,6 +934,7 @@ public class AnswerFragment extends Fragment
   @Override
   public void resetAnswerProgress() {
     affordanceHolderLayout.reset(true);
+    restoreBackgroundMaskColor();
   }
 
   private void animateEntry(@NonNull View rootView) {
@@ -992,6 +1001,10 @@ public class AnswerFragment extends Fragment
       buttonRejectClicked = true;
       answerScreenDelegate.onReject();
     }
+  }
+
+  private void restoreBackgroundMaskColor() {
+    answerScreenDelegate.updateWindowBackgroundColor(0);
   }
 
   private void restoreSwipeHintTexts() {
