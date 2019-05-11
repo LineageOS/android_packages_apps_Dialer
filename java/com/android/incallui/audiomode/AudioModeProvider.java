81e16a67c418ca43ff46a868191c7bf29be19732
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-05-11 08:33:47 +0200

    Dialer: AudioModeProvider: use wired route for usb headsets
    Change-Id: I920a9c259af5597dfd9a88a72e4ac54cb6e73f7a

--- java/com/android/incallui/audiomode/AudioModeProvider.java
+++ java/com/android/incallui/audiomode/AudioModeProvider.java
@@ -89,6 +89,7 @@ public class AudioModeProvider {
           hasBluetooth = true;
           continue;
         case AudioDeviceInfo.TYPE_WIRED_HEADSET:
+        case AudioDeviceInfo.TYPE_USB_HEADSET:
           hasHeadset = true;
           continue;
         default:
