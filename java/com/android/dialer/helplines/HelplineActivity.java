2624f0f093b21401e5dbeaa9b28ff29f2c1485ee
Michael W <baddaemon87@gmail.com>
2019-09-12 23:13:17 +0200

    WIP
    Change-Id: I952bf10ee9516b83e595d8a19450696b303393b6

---
+++ java/com/android/dialer/helplines/HelplineActivity.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.dialer.helplines;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import com.android.dialer.R;
+import com.android.server.telecom.SensitivePhoneNumberInfo;
+
+import java.util.List;
+
+public class HelplineActivity extends Activity implements LoadHelplinesTask.Callback{
+
+    private static final String KEY_FIRST_LAUNCH = "pref_first_launch";
+    public static final String SHARED_PREFERENCES_KEY = "com.android.dialer.prefs";
+
+    private RecyclerView mRecyclerView;
+    private LinearLayout mLoadingView;
+    private ProgressBar mProgressBar;
+
+    private HelplineAdapter mAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        setContentView(R.layout.activity_helplines);
+        mRecyclerView = findViewById(R.id.helplines_list);
+        mLoadingView = findViewById(R.id.helplines_loading);
+        mProgressBar = findViewById(R.id.helplines_progress_bar);
+
+        mAdapter = new HelplineAdapter(this);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter);
+
+        showUi();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_helplines, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        } else if (id == R.id.menu_helpline_help) {
+            showOnBoarding(true);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onLoadListProgress(int progress) {
+        mProgressBar.setProgress(progress);
+    }
+
+    @Override
+    public void onLoadCompleted(List<SensitivePhoneNumberInfo> result) {
+        mLoadingView.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mAdapter.update(result);
+    }
+
+    private void showUi() {
+        mLoadingView.setVisibility(View.VISIBLE);
+
+        showOnBoarding(false);
+
+        new LoadHelplinesTask(this).execute();
+    }
+
+    private void showOnBoarding(boolean forceShow) {
+        SharedPreferences preferenceManager = getPrefs();
+        if (!forceShow && preferenceManager.getBoolean(KEY_FIRST_LAUNCH, false)) {
+            return;
+        }
+
+        preferenceManager.edit()
+                .putBoolean(KEY_FIRST_LAUNCH, true)
+                .apply();
+
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.helplines_firstlaunch_title)
+                .setMessage(R.string.helplines_firstlaunch_message)
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+    }
+
+    public SharedPreferences getPrefs() {
+        return this.getSharedPreferences(SHARED_PREFERENCES_KEY,
+                Context.MODE_PRIVATE);
+    }
+}
---
+++ java/com/android/dialer/helplines/utils/HelplineUtils.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2016 The CyanogenMod project
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.dialer.helplines.utils;
+
+import android.content.res.Resources;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.android.server.telecom.SensitivePhoneNumberInfo;
+
+import java.util.ArrayList;
+
+public class HelplineUtils {
+
+    private static final String TAG = HelplineUtils.class.getSimpleName();
+    public static final String CATEGORY_STR_FORMAT = "category_%s";
+    public static final String LANGUAGE_STR_FORMAT = "language_%s";
+
+    public static String getTitle(Resources res, SensitivePhoneNumberInfo info) {
+        if (info != null) {
+            String str = getDisplayString(res, info.get("categories"), CATEGORY_STR_FORMAT);
+            if (!TextUtils.isEmpty(str)) {
+                return str;
+            }
+
+            String name = info.get("name");
+            String number = info.get("number");
+            if (!TextUtils.isEmpty(name)) {
+                return name;
+            } else if (!TextUtils.isEmpty(number)) {
+                return number;
+            }
+        }
+
+        return "";
+    }
+
+    public static String getTitle(Resources res, SensitivePhoneNumberInfo info) {
+        if (info != null) {
+            return getDisplayString(res, info.get("languages"), LANGUAGE_STR_FORMAT);
+        }
+
+        return "";
+    }
+
+    private static String getDisplayString(Resources res, String content, String pattern) {
+        if (content != null && content.length > 0) {
+            String[] listItems = TextUtils.split(content, "\\|");
+            ArrayList<String> list = new ArrayList<>();
+            for (String item : listItems) {
+                list.add(ResourceUtils.getLocalizedString(res, item, pattern));
+            }
+            if (list.size() > 0) {
+                return TextUtils.join(", ", list);
+            }
+        }
+        return "";
+    }
+}
