47220adc667b747839acd70c3fcdc5e70ccdc0f2
Michael W <baddaemon87@gmail.com>
2019-09-12 23:13:17 +0200

    WIP
    Change-Id: I952bf10ee9516b83e595d8a19450696b303393b6

---
+++ java/com/android/dialer/helplines/EmptyRecyclerView.java
@@ -0,0 +1,71 @@
+package com.android.dialer.helplines;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class EmptyRecyclerView extends RecyclerView {
+
+    private View mEmptyView;
+
+    public EmptyRecyclerView(Context context) {
+        super(context);
+    }
+
+    public EmptyRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EmptyRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    private void initEmptyView() {
+        if (mEmptyView != null) {
+            boolean isEmpty = getAdapter() == null || getAdapter().getItemCount() == 0;
+            mEmptyView.setVisibility(isEmpty ? View.VISIBLE : View.GONE);
+            EmptyRecyclerView.this.setVisibility(isEmpty ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    final AdapterDataObserver mObserver = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            initEmptyView();
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            super.onItemRangeInserted(positionStart, itemCount);
+            initEmptyView();
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            super.onItemRangeRemoved(positionStart, itemCount);
+            initEmptyView();
+        }
+    };
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+        Adapter oldAdapter = getAdapter();
+        super.setAdapter(adapter);
+
+        if (oldAdapter != null) {
+            oldAdapter.unregisterAdapterDataObserver(mObserver);
+        }
+
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(mObserver);
+        }
+    }
+
+    public void setEmptyView(View view) {
+        this.mEmptyView = view;
+        initEmptyView();
+    }
+}
---
+++ java/com/android/dialer/helplines/Helplines.java
@@ -0,0 +1,43 @@
+package com.android.dialer.helplines;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager ;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+
+import com.android.dialer.helplines.EmptyRecyclerView;
+import com.android.dialer.helplines.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Helplines extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
