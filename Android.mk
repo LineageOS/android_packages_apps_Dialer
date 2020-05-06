411a8aa96320dce06d6e38d53497b3ca51adff2c
Michael Bestas <mkbestas@lineageos.org>
2020-05-06 20:59:08 +0300

    Dialer: Add extra privapp permissions xml
    Change-Id: If15bf82ad0914ab7d1b4be5438dc860189d7bb55

--- Android.mk
+++ Android.mk
@@ -178,8 +178,18 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
 LOCAL_USE_AAPT2 := true
 LOCAL_REQUIRED_MODULES := privapp_whitelist_com.android.dialer
+LOCAL_REQUIRED_MODULES += privapp_whitelist_com.android.dialer-ext.xml
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.android.dialer-ext.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_PRODUCT_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 # Cleanup local state
 BASE_DIR :=
