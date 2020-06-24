3334d70262a74708a8fc322f16276532609a7aff
Scott Warner <Tortel1210@gmail.com>
2020-06-24 15:38:56 -0400

    Dialer: Use a DayNight launch theme
    This prevents the white launch screen from appearing when the system theme
    is dark
    
    Change-Id: I1ee8ff493910578b5e5c9de67ea1f7390178051a

--- java/com/android/dialer/main/impl/MainActivity.java
+++ java/com/android/dialer/main/impl/MainActivity.java
@@ -29,6 +29,7 @@ import com.android.dialer.interactions.PhoneNumberInteraction.InteractionErrorCo
 import com.android.dialer.interactions.PhoneNumberInteraction.InteractionErrorListener;
 import com.android.dialer.main.MainActivityPeer;
 import com.android.dialer.main.impl.bottomnav.BottomNavBar.TabIndex;
+import com.android.dialer.R;
 import com.android.dialer.util.TransactionSafeActivity;
 
 /** This is the main activity for dialer. It hosts favorites, call log, search, dialpad, etc... */
@@ -72,6 +73,7 @@ public class MainActivity extends TransactionSafeActivity
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    setTheme(R.style.MainActivityTheme);
     super.onCreate(savedInstanceState);
     LogUtil.enterBlock("MainActivity.onCreate");
     // If peer was set by the super, don't reset it.
