f209184789430ddff7df5660777097c213fd6842
nift4 <nift4@protonmail.com>
2023-08-20 17:44:14 +0200

    Dialer: add proper-ish Direct Boot support
    This is a terribly important oversight. Direct Boot lockscreens REALLY
    should support dialing 112 or whatever emergency number users use
    without the Dialer app crashing...
    
    Change-Id: Iaafadf7d29352c53b29ce7db1ebd5bfb651fa49b

--- java/com/android/dialer/app/settings/SoundSettingsFragment.java
+++ java/com/android/dialer/app/settings/SoundSettingsFragment.java
@@ -87,6 +87,7 @@ public class SoundSettingsFragment extends PreferenceFragment
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    getPreferenceManager().setStorageDeviceProtected();
 
     addPreferencesFromResource(R.xml.sound_settings);
 
