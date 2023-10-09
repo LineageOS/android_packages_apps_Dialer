27d3e70901fff6d73643db3539e3328e90213a6a
Michael W <baddaemon87@gmail.com>
2023-10-09 22:53:31 +0200

    Dialer: Style Settings
    Change-Id: I674c12ede4ab465b22cb3959dbeeb4b52647b8be

--- java/com/android/dialer/app/settings/PhoneAccountSelectionFragment.java
+++ java/com/android/dialer/app/settings/PhoneAccountSelectionFragment.java
@@ -93,6 +93,8 @@ public class PhoneAccountSelectionFragment extends PreferenceFragmentCompat {
       Icon icon = phoneAccount.getIcon();
       if (icon != null) {
         setIcon(icon.loadDrawable(context));
+      } else {
+        setIconSpaceReserved(false);
       }
     }
 
