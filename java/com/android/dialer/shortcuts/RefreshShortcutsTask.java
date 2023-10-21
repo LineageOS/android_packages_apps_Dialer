e99397357cec71a5b832ba5906b2e29e31d80929
Michael W <baddaemon87@gmail.com>
2023-10-21 17:38:03 +0200

    Dialer: Replace AsyncTask(Loader)s
    They are deprecated
    
    Change-Id: Ie927304e66edab39465850954fc380e8c0c4cfed

--- java/com/android/dialer/shortcuts/RefreshShortcutsTask.java
+++ java/com/android/dialer/shortcuts/RefreshShortcutsTask.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2023 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,53 +19,57 @@ package com.android.dialer.shortcuts;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
-import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 
-import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
-import androidx.annotation.WorkerThread;
 
-import com.android.dialer.common.Assert;
 import com.android.dialer.common.LogUtil;
 
-/** {@link AsyncTask} used by the periodic job service to refresh dynamic and pinned shortcuts. */
-final class RefreshShortcutsTask extends AsyncTask<JobParameters, Void, JobParameters> {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+/** Class used by the periodic job service to refresh dynamic and pinned shortcuts. */
+final class RefreshShortcutsTask {
 
   private final JobService jobService;
+  private final ExecutorService executor = Executors.newSingleThreadExecutor();
+  private final Handler handler = new Handler(Looper.getMainLooper());
+
+  private boolean mIsCancelled = false;
 
   RefreshShortcutsTask(@NonNull JobService jobService) {
     this.jobService = jobService;
   }
 
-  /** @param params array with length 1, provided from PeriodicJobService */
-  @Override
-  @NonNull
-  @WorkerThread
-  protected JobParameters doInBackground(JobParameters... params) {
-    Assert.isWorkerThread();
-    LogUtil.enterBlock("RefreshShortcutsTask.doInBackground");
+  /** @param param provided from PeriodicJobService */
+  public void execute(JobParameters param) {
+    executor.execute(() -> {
+      LogUtil.enterBlock("RefreshShortcutsTask.doInBackground");
 
-    // Dynamic shortcuts are refreshed from the UI but icons can become stale, so update them
-    // periodically using the job service.
-    //
-    // The reason that icons can become is stale is that there is no last updated timestamp for
-    // pictures; there is only a last updated timestamp for the entire contact row, which changes
-    // frequently (for example, when they are called their "times_contacted" is incremented).
-    // Relying on such a spuriously updated timestamp would result in too frequent shortcut updates,
-    // so instead we just allow the icon to become stale in the case that the contact's photo is
-    // updated, and then rely on the job service to periodically force update it.
-    new DynamicShortcuts(jobService, new IconFactory(jobService)).updateIcons(); // Blocking
-    new PinnedShortcuts(jobService).refresh(); // Blocking
+      // Dynamic shortcuts are refreshed from the UI but icons can become stale, so update them
+      // periodically using the job service.
+      //
+      // The reason that icons can become is stale is that there is no last updated timestamp for
+      // pictures; there is only a last updated timestamp for the entire contact row, which changes
+      // frequently (for example, when they are called their "times_contacted" is incremented).
+      // Relying on such a spuriously updated timestamp would result in too frequent shortcut
+      // updates, so instead we just allow the icon to become stale in the case that the contact's
+      // photo is updated, and then rely on the job service to periodically force update it.
+      new DynamicShortcuts(jobService, new IconFactory(jobService)).updateIcons(); // Blocking
+      new PinnedShortcuts(jobService).refresh(); // Blocking
 
-    return params[0];
+      if (!mIsCancelled) {
+        handler.post(() -> {
+          LogUtil.enterBlock("RefreshShortcutsTask.onPostExecute");
+          jobService.jobFinished(param, false /* needsReschedule */);
+        });
+      }
+    });
   }
 
-  @Override
-  @MainThread
-  protected void onPostExecute(JobParameters params) {
-    Assert.isMainThread();
-    LogUtil.enterBlock("RefreshShortcutsTask.onPostExecute");
-
-    jobService.jobFinished(params, false /* needsReschedule */);
+  public void cancel() {
+    mIsCancelled = true;
   }
 }
