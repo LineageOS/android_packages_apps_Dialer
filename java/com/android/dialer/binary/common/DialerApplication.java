2a558d119224adc2d8a62071db08144c02032001
Michael W <baddaemon87@gmail.com>
2023-10-27 15:52:16 +0200

    Dialer: Remove persistent logger
    * We don't use it
    * Simulator: crashed on sharing anyway
    * Generally not worth keeping
    
    Change-Id: I5bd966b84997405dc884e7c6b800002770bbe1b7

--- java/com/android/dialer/binary/common/DialerApplication.java
+++ java/com/android/dialer/binary/common/DialerApplication.java
@@ -23,7 +23,6 @@ import androidx.annotation.NonNull;
 
 import com.android.dialer.inject.HasRootComponent;
 import com.android.dialer.notification.NotificationChannelManager;
-import com.android.dialer.persistentlog.PersistentLogger;
 
 /** A common application subclass for all Dialer build variants. */
 public abstract class DialerApplication extends Application implements HasRootComponent {
@@ -34,7 +33,6 @@ public abstract class DialerApplication extends Application implements HasRootCo
   public void onCreate() {
     Trace.beginSection("DialerApplication.onCreate");
     super.onCreate();
-    PersistentLogger.initialize(this);
     NotificationChannelManager.initChannels(this);
     Trace.endSection();
   }
