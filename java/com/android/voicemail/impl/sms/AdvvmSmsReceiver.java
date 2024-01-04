528a035a2211538f25b74f0834c1cb06c36f663c
Joel Beckmeyer <jbeckmeyer@cedarville.edu>
2024-01-04 16:20:16 -0500

    [WIP] Dialer: implement vvm_type_advvm (ATT Visual Voicemail)
    Change-Id: If1ddf1fba719e876910943759b90d1a1fdd1593d

---
+++ java/com/android/voicemail/impl/sms/AdvvmSmsReceiver.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.voicemail.impl.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Telephony;
+import android.provider.VoicemailContract;
+import android.telecom.PhoneAccountHandle;
+import android.telephony.SmsMessage;
+import com.android.voicemail.impl.VvmLog;
+import com.android.voicemail.impl.sync.SyncTask;
+
+/** Receive data SMS messages for the ADVVM protocol used by AT&T and others. */
+public class AdvvmSmsReceiver extends BroadcastReceiver {
+
+  private static final String TAG = "AdvvmSmsReceiver";
+
+  private Context context;
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    this.context = context;
+    VvmLog.i(TAG, "Received data SMS on port 5499, printing...");
+    SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+    for (SmsMessage msg: messages) {
+      Uri bodyUri = Uri.parse("advvm://" + msg.getMessageBody());
+      for (String query: bodyUri.getQueryParameterNames()) {
+        VvmLog.i(TAG, query + " = " + bodyUri.getQueryParameter(query));
+      }
+      String phoneString = bodyUri.getQueryParameter("m");
+      PhoneAccountHandle phone = new PhoneAccountHandle(null, phoneString);
+      SyncTask.start(context, phone);
+    }
+    VvmLog.i(TAG, "Received data SMS on port 5499, done printing...");
+  }
+}
