f653e40517c914892db1ad31066a23d3a3e71fe7
Georg Veichtlbauer <georg@vware.at>
2024-02-17 14:06:07 +0100

    Dialer: Update icons with new Material Symbols
    Change-Id: Ic80b15d9ee73fac461298a49de4ff62713553f37

--- java/com/android/incallui/speakerbuttonlogic/SpeakerButtonInfo.java
+++ java/com/android/incallui/speakerbuttonlogic/SpeakerButtonInfo.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,7 +59,7 @@ public class SpeakerButtonInfo {
 
       if ((audioState.getRoute() & CallAudioState.ROUTE_BLUETOOTH)
           == CallAudioState.ROUTE_BLUETOOTH) {
-        icon = R.drawable.volume_bluetooth;
+        icon = R.drawable.quantum_ic_phone_bluetooth_vd_theme_24;
         contentDescription = R.string.incall_content_description_bluetooth;
         label = R.string.audioroute_bluetooth;
         deviceName = BluetoothUtil.getAliasName(audioState.getActiveBluetoothDevice());
